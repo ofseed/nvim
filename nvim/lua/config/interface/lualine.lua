fc7decb6bd8a5f84d12bd0081c2d8cf588e706de
Yi Ming <ofseed@foxmail.com>
2021-08-29 15:44:48 +0800

    Completed lualine configure

--- nvim/lua/config/interface/lualine.lua
+++ nvim/lua/config/interface/lualine.lua
@@ -2,6 +2,12 @@ local vim = vim
 
 require('lualine').setup {
   options = {
-    theme = vim.g.colors_name
+    theme = vim.g.colors_name,
+    disabled_filetypes = {
+      'dashboard'
+    }
+  },
+  extensions = {
+    'nvim-tree',
   }
 }
