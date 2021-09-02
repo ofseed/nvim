958160155a532a9c4b47467c3b9d987fa56689f4
Yi Ming <ofseed@foxmail.com>
2021-09-02 17:09:40 +0800

    Modified lualine seperators

--- nvim/lua/config/interface/lualine.lua
+++ nvim/lua/config/interface/lualine.lua
@@ -6,7 +6,9 @@ lualine.setup {
     theme = vim.g.colors_name,
     disabled_filetypes = {
       'dashboard'
-    }
+    },
+    section_separators = {'', ''},
+    component_separators = {'', ''}
   },
   extensions = {
     'nvim-tree',
