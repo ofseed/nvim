0231d313f8262d1f90b6d1cf4f14235b73c508a0
Yi Ming <ofseed@foxmail.com>
2021-11-25 22:47:45 +0800

    Add cmp-nvim-lsp-signature-help

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -387,6 +387,7 @@ return packer.startup {
       requires = {
         { "hrsh7th/cmp-buffer" },
         { "hrsh7th/cmp-nvim-lsp" },
+        { "hrsh7th/cmp-nvim-lsp-signature-help" },
         { "hrsh7th/cmp-buffer" },
         { "hrsh7th/cmp-path" },
         { "hrsh7th/cmp-cmdline" },
