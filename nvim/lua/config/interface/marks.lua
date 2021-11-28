ec7928ed871981688f2e400ebc4511804694ecb9
Yi Ming <ofseed@foxmail.com>
2021-11-28 16:56:58 +0800

    Add NvimTree and Outline to marks.nvim.exclued_filetypes

--- nvim/lua/config/interface/marks.lua
+++ nvim/lua/config/interface/marks.lua
@@ -20,7 +20,10 @@ marks.setup {
   -- default 10.
   sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
   -- disables mark tracking for specific filetypes. default {}
-  excluded_filetypes = {},
+  excluded_filetypes = {
+    "NvimTree",
+    "Outline",
+  },
   -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
   -- sign/virttext. Bookmarks can be used to group together positions and quickly move
   -- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
