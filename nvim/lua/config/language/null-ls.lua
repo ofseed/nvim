058516b97351f12461fa1346a59b4e70130f6970
Yi Ming <ofseed@foxmail.com>
2021-12-14 00:10:58 +0800

    Add autopep8 formatter in null-ls

--- nvim/lua/config/language/null-ls.lua
+++ nvim/lua/config/language/null-ls.lua
@@ -3,6 +3,7 @@ local ls = require "null-ls"
 ls.setup {
   sources = {
     ls.builtins.formatting.stylua,
+    ls.builtins.formatting.autopep8,
     ls.builtins.completion.spell,
     ls.builtins.formatting.codespell,
     ls.builtins.diagnostics.codespell,
