895325c25654d94bcac8facc93a1ee41c9f7102d
Yi Ming <ofseed@foxmail.com>
2021-12-19 06:22:05 +0800

    Disable codespell formatter and spell completion

--- nvim/lua/config/language/null-ls.lua
+++ nvim/lua/config/language/null-ls.lua
@@ -4,8 +4,6 @@ ls.setup {
   sources = {
     ls.builtins.formatting.stylua,
     ls.builtins.formatting.autopep8,
-    ls.builtins.completion.spell,
-    ls.builtins.formatting.codespell,
     ls.builtins.diagnostics.codespell,
   },
 }
