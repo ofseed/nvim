d81b6283d57a7d91a23dad3e46eb066383cf2750
Yi Ming <ofseed@foxmail.com>
2021-12-21 13:26:17 +0800

    Update which-key config

--- nvim/lua/config/interface/which-key.lua
+++ nvim/lua/config/interface/which-key.lua
@@ -35,11 +35,16 @@ key.setup {
     separator = "➜", -- symbol used between a key and it's label
     group = "+", -- symbol prepended to a group
   },
+  popup_mappings = {
+    scroll_down = "<c-d>", -- binding to scroll down inside the popup
+    scroll_up = "<c-u>", -- binding to scroll up inside the popup
+  },
   window = {
     border = "none", -- none, single, double, shadow
     position = "bottom", -- bottom, top
     margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
     padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+    winblend = 0,
   },
   layout = {
     height = { min = 4, max = 25 }, -- min and max height of the columns
