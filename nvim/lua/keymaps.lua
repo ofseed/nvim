9fc7ddad2b614b67e025e8b7555982b2e9a21379
Yi Ming <ofseed@foxmail.com>
2021-12-21 17:00:25 +0800

    Add Diffview keybindings

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -11,4 +11,10 @@ vim.api.nvim_set_keymap("n", "<leader>tw", ":Twilight<CR>", { noremap = true, si
 
 vim.api.nvim_set_keymap("n", "<leader>ng", ":Neogit<CR>", { noremap = true, silent = true })
 
+vim.api.nvim_set_keymap("n", "<leader>dvo", ":DiffviewOpen<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "<leader>dvh", ":DiffviewFileHistory<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "<leader>dvc", ":DiffviewClose<CR>", { noremap = true, silent = true })
+
 -- vim.api.nvim_set_keymap("n", "<leader>ca", ":CodeActionMenu<CR>", { noremap = true, silent = true })
