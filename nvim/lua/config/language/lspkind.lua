a2375e0fb98d8994cece0854b97d4bb2f534858b
Yi Ming <ofseed@foxmail.com>
2022-02-04 23:08:47 +0800

    Adapt lspkind to new vewrsion

--- nvim/lua/config/language/lspkind.lua
+++ nvim/lua/config/language/lspkind.lua
@@ -1,17 +1,22 @@
 local lspkind = require "lspkind"
 
 lspkind.init {
-  -- enables text annotations
+  -- DEPRECATED (use mode instead): enables text annotations
   --
   -- default: true
-  with_text = true,
+  -- with_text = true,
+
+  -- defines how annotations are shown
+  -- default: symbol
+  -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+  mode = "symbol_text",
 
   -- default symbol map
   -- can be either 'default' (requires nerd-fonts font) or
   -- 'codicons' for codicon preset (requires vscode-codicons font)
   --
   -- default: 'default'
-  preset = "default",
+  preset = "codicons",
 
   -- override preset symbols
   --
