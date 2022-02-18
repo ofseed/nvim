7aca56b9ff10bfed88a2896208a1b2d067439134
Yi Ming <ofseed@foxmail.com>
2022-02-18 20:59:34 +0800

    Enable friendly-snippets in nvim-cmp

--- nvim/lua/config/language/nvim-cmp.lua
+++ nvim/lua/config/language/nvim-cmp.lua
@@ -48,7 +48,7 @@ cmp.setup {
   sources = {
     { name = "nvim_lsp" },
     { name = "nvim_lua" },
-    -- { name = "luasnip" },
+    { name = "luasnip" },
     { name = "buffer" },
     { name = "path" },
     { name = "calc" },
