997f5634d77d95f8bb2f817b2a540203b134e7a8
Yi Ming <ofseed@foxmail.com>
2022-02-28 14:27:42 +0800

    Add a hook function in kommentary for vue support

--- nvim/lua/config/tools/kommentary.lua
+++ nvim/lua/config/tools/kommentary.lua
@@ -14,3 +14,11 @@ config.configure_language("html", {
     require("ts_context_commentstring.internal").update_commentstring()
   end,
 })
+
+config.configure_language("vue", {
+  single_line_comment_string = "auto",
+  multi_line_comment_strings = "auto",
+  hook_function = function()
+    require("ts_context_commentstring.internal").update_commentstring()
+  end,
+})
