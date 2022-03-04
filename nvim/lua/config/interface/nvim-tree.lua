e8e9588faef41010ca34e6dab423664d95a11c47
Yi Ming <ofseed@foxmail.com>
2022-03-04 13:09:17 +0800

    Set nvim-tree min window width to 25

--- nvim/lua/config/interface/nvim-tree.lua
+++ nvim/lua/config/interface/nvim-tree.lua
@@ -110,7 +110,7 @@ tree.setup {
   view = {
     width = function()
       local columns = vim.go.columns
-      return math.floor(columns * 0.2)
+      return math.floor(columns * 0.2) > 25 and math.floor(columns * 0.2) or 25
     end,
     height = 30,
     hide_root_folder = false,
