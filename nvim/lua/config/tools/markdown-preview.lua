b871a420a4a6d751c1f2c60be9b7d77a7c525d93
Yi Ming <ofseed@foxmail.com>
2022-03-09 01:19:33 +0800

    Change markdown-preview browser-open command

--- nvim/lua/config/tools/markdown-preview.lua
+++ nvim/lua/config/tools/markdown-preview.lua
@@ -33,7 +33,7 @@ vim.g.mkdp_open_ip = ""
 
 -- specify browser to open preview page
 -- default: ''
-vim.g.mkdp_browser = "microsoft-edge"
+vim.g.mkdp_browser = "microsoft-edge-stable"
 
 -- set to 1, echo preview page url in command line when open preview page
 -- default is 0
