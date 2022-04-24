2811365875f1ea7d1521bc9f5d0b38ea7d538719
Yi Ming <ofseed@foxmail.com>
2022-04-24 13:00:24 +0800

    Update neogit config

--- lua/tool/neogit.lua
+++ lua/tool/neogit.lua
@@ -8,9 +8,12 @@ neogit.setup {
   disable_signs = false,
   disable_hint = false,
   disable_context_highlighting = false,
-  disable_commit_confirmation = false,
+  disable_commit_confirmation = true,
+  -- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
+  -- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
   auto_refresh = true,
   disable_builtin_notifications = false,
+  use_magit_keybindings = false,
   commit_popup = {
     kind = "split",
   },
