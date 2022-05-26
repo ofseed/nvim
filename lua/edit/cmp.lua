2b14d623378a0cc73b436f7bcafbdffcb4203afd
Yi Ming <ofseed@foxmail.com>
2022-05-26 16:36:27 +0800

    Update cmp config

--- lua/edit/cmp.lua
+++ lua/edit/cmp.lua
@@ -16,6 +16,11 @@ if not ok then
   return
 end
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
 cmp.setup {
   window = {
     completion = {
@@ -63,7 +68,9 @@ cmp.setup {
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
@@ -75,7 +82,7 @@ cmp.setup {
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+        luasnip.jump(-1)
       else
         fallback()
       end
