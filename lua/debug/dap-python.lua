fb16746641383ed10f5591d8ffbad9715e6ce415
Yi Ming <ofseed@foxmail.com>
2022-05-29 02:15:36 +0800

    Reconfigure dap-python

--- lua/debug/dap-python.lua
+++ lua/debug/dap-python.lua
@@ -1,11 +1,17 @@
-local ok, python = pcall(require, "dap-python")
+local ok, dap = pcall(require, "dap-python")
 if not ok then
-  vim.notify "Could not load dap python"
+  vim.notify "Could not load dap-python"
   return
 end
 
-python.setup "/usr/bin/python3"
+dap.setup "/usr/bin/python3"
 
-vim.keymap.set("n", "<leader>dn", ":lua require('dap-python').test_method()<CR>")
-vim.keymap.set("n", "<leader>df", ":lua require('dap-python').test_class()<CR>")
-vim.keymap.set("v", "<leader>ds", "<ESC>:lua require('dap-python').debug_selection()<CR>")
+vim.keymap.set("n", "<leader>dn", function()
+  require("dap-python").test_method()
+end)
+vim.keymap.set("n", "<leader>df", function()
+  require("dap-python").test_class()
+end)
+vim.keymap.set("v", "<leader>ds", function()
+  require("dap-python").debug_selection()
+end)
