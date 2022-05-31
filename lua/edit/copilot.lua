9c9c72fe251f685968d9140b07d29293a7c00801
Yi Ming <ofseed@foxmail.com>
2022-05-31 19:20:55 +0800

    Add a keybind for copilot panel

--- lua/edit/copilot.lua
+++ lua/edit/copilot.lua
@@ -3,4 +3,5 @@ vim.g.copilot_filetypes = {
 }
 
 vim.keymap.set("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, script = true, expr = true })
+vim.keymap.set("i", "<M-NL>", '<Cmd>Copilot<CR>')
 vim.g.copilot_no_tab_map = true
