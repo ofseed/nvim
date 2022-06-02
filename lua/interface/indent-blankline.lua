e792e281bcf6cec406df971922cf6f4ea7710043
Yi Ming <ofseed@foxmail.com>
2022-06-02 11:28:24 +0800

    Update indent-blankline config

--- lua/interface/indent-blankline.lua
+++ lua/interface/indent-blankline.lua
@@ -1,36 +1,11 @@
-vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-vim.g.indent_blankline_filetype_exclude = {
-  "help",
-  "startify",
-  "alpha",
-  "packer",
-  "neogitstatus",
-  "NvimTree",
-  "Trouble",
-}
-vim.g.indent_blankline_char = "│"
-vim.g.indent_blankline_use_treesitter = true
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_show_current_context = true
-vim.g.indent_blankline_context_patterns = {
-  "class",
-  "return",
-  "function",
-  "method",
-  "^if",
-  "^while",
-  "jsx_element",
-  "^for",
-  "^object",
-  "^table",
-  "block",
-  "arguments",
-  "if_statement",
-  "else_clause",
-  "jsx_element",
-  "jsx_self_closing_element",
-  "try_statement",
-  "catch_clause",
-  "import_statement",
-  "operation_type",
+local ok, blankline = pcall(require, "indent_blankline")
+if not ok then
+  vim.notify "Could not load indent blankline"
+end
+
+blankline.setup {
+  show_current_context = true,
+  show_current_context_start = true,
+  -- use_treesitter = true,
+  -- use_treesitter_scope = true,
 }
