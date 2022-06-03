7bfaf861d094c6f73eacff512ad09c7f51b0cd27
Yi Ming <ofseed@foxmail.com>
2022-06-04 01:41:56 +0800

    Add description for bufferline keymaps

--- lua/interface/bufferline.lua
+++ lua/interface/bufferline.lua
@@ -84,20 +84,22 @@ bufferline.setup {
   },
 }
 
-vim.keymap.set("n", "]b", "<cmd>BufferLineCycleNext<CR>")
-vim.keymap.set("n", "[b", "<cmd>BufferLineCyclePrev<CR>")
-vim.keymap.set("n", "gb", "<cmd>BufferLinePick<CR>")
-vim.keymap.set("n", "<M-1>", "<Cmd>BufferLineGoToBuffer 1<CR>")
-vim.keymap.set("n", "<M-2>", "<Cmd>BufferLineGoToBuffer 2<CR>")
-vim.keymap.set("n", "<M-3>", "<Cmd>BufferLineGoToBuffer 3<CR>")
-vim.keymap.set("n", "<M-4>", "<Cmd>BufferLineGoToBuffer 4<CR>")
-vim.keymap.set("n", "<M-5>", "<Cmd>BufferLineGoToBuffer 5<CR>")
-vim.keymap.set("n", "<M-6>", "<Cmd>BufferLineGoToBuffer 6<CR>")
-vim.keymap.set("n", "<M-7>", "<Cmd>BufferLineGoToBuffer 7<CR>")
-vim.keymap.set("n", "<M-8>", "<Cmd>BufferLineGoToBuffer 8<CR>")
-vim.keymap.set("n", "<M-9>", "<Cmd>BufferLineGoToBuffer 9<CR>")
--- TODO: conflict with nvim-dap
-vim.keymap.set("n", "<leader>bc", "<cmd>BufferLinePickClose<CR>")
-vim.keymap.set("n", "<leader>bse", "<cmd>BufferLineSortByExtension<CR>")
-vim.keymap.set("n", "<leader>bsd", "<cmd>BufferLineSortByDirectory<CR>")
-vim.keymap.set("n", "<leader>bst", "<cmd>BufferLineSortByTabs<CR>")
+vim.keymap.set("n", "<M-1>", "<Cmd>BufferLineGoToBuffer 1<CR>", { desc = "Go to buffer 1" })
+vim.keymap.set("n", "<M-2>", "<Cmd>BufferLineGoToBuffer 2<CR>", { desc = "Go to buffer 2" })
+vim.keymap.set("n", "<M-3>", "<Cmd>BufferLineGoToBuffer 3<CR>", { desc = "Go to buffer 3" })
+vim.keymap.set("n", "<M-4>", "<Cmd>BufferLineGoToBuffer 4<CR>", { desc = "Go to buffer 4" })
+vim.keymap.set("n", "<M-5>", "<Cmd>BufferLineGoToBuffer 5<CR>", { desc = "Go to buffer 5" })
+vim.keymap.set("n", "<M-6>", "<Cmd>BufferLineGoToBuffer 6<CR>", { desc = "Go to buffer 6" })
+vim.keymap.set("n", "<M-7>", "<Cmd>BufferLineGoToBuffer 7<CR>", { desc = "Go to buffer 7" })
+vim.keymap.set("n", "<M-8>", "<Cmd>BufferLineGoToBuffer 8<CR>", { desc = "Go to buffer 8" })
+vim.keymap.set("n", "<M-9>", "<Cmd>BufferLineGoToBuffer 9<CR>", { desc = "Go to buffer 9" })
+
+vim.keymap.set("n", "]b", "<cmd>BufferLineCycleNext<CR>", { desc = "Buffer" })
+vim.keymap.set("n", "[b", "<cmd>BufferLineCyclePrev<CR>", { desc = "Buffer" })
+
+vim.keymap.set("n", "gb", "<cmd>BufferLinePick<CR>", { desc = "Buffer" })
+
+vim.keymap.set("n", "<leader>bc", "<cmd>BufferLinePickClose<CR>", { desc = "Close" })
+vim.keymap.set("n", "<leader>bse", "<cmd>BufferLineSortByExtension<CR>", { desc = "By extension" })
+vim.keymap.set("n", "<leader>bsd", "<cmd>BufferLineSortByDirectory<CR>", { desc = "By directory" })
+vim.keymap.set("n", "<leader>bst", "<cmd>BufferLineSortByTabs<CR>", { desc = "By tabs" })
