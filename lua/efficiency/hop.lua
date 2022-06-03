ac70b05b769bc7f963cacd420e5f77bf200f9dfd
Yi Ming <ofseed@foxmail.com>
2022-06-04 02:53:06 +0800

    Modify hop keymaps

--- lua/efficiency/hop.lua
+++ lua/efficiency/hop.lua
@@ -9,20 +9,23 @@ hop.setup {
   jump_on_sole_occurrence = false,
 }
 
-vim.keymap.set({ "n", "v" }, "sf", "<Cmd>HopChar1AC<CR>") -- Case ignored
-vim.keymap.set({ "n", "v", "o" }, "<Leader><Leader>f", "<Cmd>HopChar1AC<CR>")
--- t, T not implemented yet
-vim.keymap.set({ "n", "v" }, "sw", "<Cmd>HopWordAC<CR>")
-vim.keymap.set({ "n", "v", "o" }, "<Leader><Leader>w", "<Cmd>HopWordAC<CR>")
--- W not implemented yet
-vim.keymap.set({ "n", "v" }, "sb", "<Cmd>HopWordBC<CR>")
-vim.keymap.set({ "n", "v", "o" }, "<Leader><Leader>b", "<Cmd>HopWordBC<CR>")
--- B, e, E, ge, gE not implemented yet
-vim.keymap.set({ "n", "v" }, "sj", "<Cmd>HopLineStartAC<CR>")
-vim.keymap.set({ "n", "v", "o" }, "<Leader><Leader>j", "<Cmd>HopLineStartAC<CR>")
+vim.keymap.set({ "n", "v" }, "sf", "<Cmd>HopChar1AC<CR>", { desc = "Char after" }) -- Case ignored
+vim.keymap.set({ "n", "v", "o" }, "<Leader>hf", "<Cmd>HopChar1AC<CR>", { desc = "Char after" })
 
-vim.keymap.set({ "n", "v" }, "sk", "<Cmd>HopLineStartBC<CR>")
-vim.keymap.set({ "n", "v", "o" }, "<Leader><Leader>k", "<Cmd>HopLineStartBC<CR>")
+vim.keymap.set({ "n", "v" }, "sF", "<Cmd>HopChar1BC<CR>", { desc = "Char after" }) -- Case ignored
+vim.keymap.set({ "n", "v", "o" }, "<Leader>hF", "<Cmd>HopChar1BC<CR>", { desc = "Char after" })
+-- t, T not implemented yet
+vim.keymap.set({ "n", "v" }, "sw", "<Cmd>HopWordAC<CR>", { desc = "Word" })
+vim.keymap.set({ "n", "v", "o" }, "<Leader>hw", "<Cmd>HopWordAC<CR>", { desc = "Word" })
+-- W not implemented yet
+vim.keymap.set({ "n", "v" }, "sb", "<Cmd>HopWordBC<CR>", { desc = "Word reverse" })
+vim.keymap.set({ "n", "v", "o" }, "<Leader>hb", "<Cmd>HopWordBC<CR>", { desc = "Word reverse" })
+-- B, e, E, ge, gE not implemented yet
+vim.keymap.set({ "n", "v" }, "sj", "<Cmd>HopLineStartAC<CR>", { desc = "Line after" })
+vim.keymap.set({ "n", "v", "o" }, "<Leader>hj", "<Cmd>HopLineStartAC<CR>", { desc = "Line after" })
+
+vim.keymap.set({ "n", "v" }, "sk", "<Cmd>HopLineStartBC<CR>", { desc = "Line before" })
+vim.keymap.set({ "n", "v", "o" }, "<Leader>hk", "<Cmd>HopLineStartBC<CR>", { desc = "Line before" })
 -- n, N not implemented yet
-vim.keymap.set({ "n", "v" }, "ss", "<Cmd>HopChar1MW<CR>")
-vim.keymap.set({ "n", "v", "o" }, "<Leader><Leader>s", "<Cmd>HopChar1MW<CR>")
+vim.keymap.set({ "n", "v" }, "ss", "<Cmd>HopChar1MW<CR>", { desc = "Char" })
+vim.keymap.set({ "n", "v", "o" }, "<Leader>hs", "<Cmd>HopChar1MW<CR>", { desc = "Char" })
