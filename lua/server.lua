ea37cd6a6459c4546ad853d95167325c667b0cfb
Yi Ming <ofseed@foxmail.com>
2022-06-04 01:15:23 +0800

    Reset LSP keymaps

--- lua/server.lua
+++ lua/server.lua
@@ -26,29 +26,39 @@ M.on_attach = function(client, bufnr)
   local bufopts = { silent = true, buffer = bufnr }
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, bufopts)
-  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, bufopts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP Hover" })
+  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { buffer = bufnr, desc = "LSP Signature help" })
 
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set("n", "gI", vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { buffer = bufnr, desc = "Diagnostic" })
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { buffer = bufnr, desc = "Diagnostic" })
 
-  vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, bufopts)
-  vim.keymap.set("n", "<leader>F", vim.lsp.buf.formatting, bufopts)
-  vim.keymap.set("n", "<leader>wl", function()
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "Definition" })
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "Declaration" })
+  vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { buffer = bufnr, desc = "Implementation" })
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = bufnr, desc = "References" })
+
+  vim.keymap.set("n", "<leader>F", vim.lsp.buf.formatting, { buffer = bufnr, desc = "Format document" })
+
+  vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename" })
+  vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code action" })
+  vim.keymap.set("n", "<leader>ll", vim.diagnostic.setloclist, { buffer = bufnr, desc = "Diagnostic list" })
+  vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { buffer = bufnr, desc = "Diagnostic float" })
+
+  vim.keymap.set(
+    "n",
+    "<leader>lwa",
+    vim.lsp.buf.add_workspace_folder,
+    { buffer = bufnr, desc = "Add workspace folder" }
+  )
+  vim.keymap.set(
+    "n",
+    "<leader>lwr",
+    vim.lsp.buf.remove_workspace_folder,
+    { buffer = bufnr, desc = "Remove workspace folder" }
+  )
+  vim.keymap.set("n", "<leader>lwl", function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set("n", "<leader>dd", function()
-    vim.diagnostic.open_float { source = true }
-  end, bufopts)
+  end, { buffer = bufnr, desc = "List workspace folders" })
 
   illuminate.on_attach(client)
   aerial.on_attach(client)
