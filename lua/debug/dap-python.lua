763faac88906a7290d454f0ecc3c6ed92df6d60c
Yi Ming <ofseed@foxmail.com>
2022-06-03 22:12:31 +0800

    Remove redundant code about keymaps

--- lua/debug/dap-python.lua
+++ lua/debug/dap-python.lua
@@ -6,12 +6,6 @@ end
 
 dap.setup "/usr/bin/python3"
 
-vim.keymap.set("n", "<leader>dn", function()
-  require("dap-python").test_method()
-end)
-vim.keymap.set("n", "<leader>df", function()
-  require("dap-python").test_class()
-end)
-vim.keymap.set("v", "<leader>ds", function()
-  require("dap-python").debug_selection()
-end)
+vim.keymap.set("n", "<leader>dn", require("dap-python").test_method)
+vim.keymap.set("n", "<leader>df", require("dap-python").test_class)
+vim.keymap.set("v", "<leader>ds", require("dap-python").debug_selection)
