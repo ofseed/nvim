f1dc4db65918811354189e0d3f00352ba37c5f67
Yi Ming <ofseed@foxmail.com>
2022-06-14 10:22:12 +0800

    Change aerial backend priority

--- lua/tool/aerial.lua
+++ lua/tool/aerial.lua
@@ -7,7 +7,7 @@ end
 aerial.setup {
   -- Priority list of preferred backends for aerial.
   -- This can be a filetype map (see :help aerial-filetype-map)
-  backends = { "treesitter", "lsp", "markdown" },
+  backends = { "lsp", "treesitter", "markdown" },
 
   -- Enum: persist, close, auto, global
   --   persist - aerial window will stay open until closed
