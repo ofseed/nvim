943c91ccbdf1928f627509aa15f414ec6bd4dc70
Yi Ming <ofseed@foxmail.com>
2022-07-16 15:55:49 +0800

    Only shows modified buffers in bufferline

--- lua/interface/bufferline.lua
+++ lua/interface/bufferline.lua
@@ -42,21 +42,7 @@ bufferline.setup {
     end,
     -- NOTE: this will be called a lot so don't do any heavy processing here
     custom_filter = function(buf_number, buf_numbers)
-      -- filter out filetypes you don't want to see
-      if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-        return true
-      end
-      -- filter out by buffer name
-      if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-        return true
-      end
-      -- filter out based on arbitrary rules
-      -- e.g. filter out vim wiki buffer from tabline in your work repo
-      if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-        return true
-      end
-      -- filter out by it's index number in list (don't show first buffer)
-      if buf_numbers[1] ~= buf_number then
+      if buf_number == vim.api.nvim_get_current_buf() or vim.bo[buf_number].modified then
         return true
       end
     end,
