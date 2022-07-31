7e076639a6f1258ae885510c01b1a4a1de3d5686
Yi Ming <ofseed@foxmail.com>
2022-08-01 05:27:08 +0800

    Do not hide dotfiles in neo-tree

--- lua/tool/neo-tree.lua
+++ lua/tool/neo-tree.lua
@@ -127,7 +127,7 @@ tree.setup {
   filesystem = {
     filtered_items = {
       visible = false, -- when true, they will just be displayed differently than normal items
-      hide_dotfiles = true,
+      hide_dotfiles = false,
       hide_gitignored = true,
       hide_hidden = true, -- only works on Windows for hidden files/directories
       hide_by_name = {
