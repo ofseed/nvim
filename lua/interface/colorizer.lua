474459535e46b58ce30b7792a996c42bdc904a88
Yi Ming <ofseed@foxmail.com>
2022-07-31 20:23:52 +0800

    Configure colorizer

--- lua/interface/colorizer.lua
+++ lua/interface/colorizer.lua
@@ -4,4 +4,20 @@ if not ok then
   return
 end
 
-colorizer.setup {}
+colorizer.setup({
+  "html",
+  "css",
+  "javascript",
+  "vue",
+}, {
+  RGB = true, -- #RGB hex codes
+  RRGGBB = true, -- #RRGGBB hex codes
+  names = true, -- "Name" codes like Blue
+  RRGGBBAA = true, -- #RRGGBBAA hex codes
+  rgb_fn = true, -- CSS rgb() and rgba() functions
+  hsl_fn = true, -- CSS hsl() and hsla() functions
+  css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+  css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+  -- Available modes: foreground, background
+  mode = "background", -- Set the display mode
+})
