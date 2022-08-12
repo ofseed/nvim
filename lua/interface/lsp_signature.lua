9a2ce16d96fafc7b8c5294152b6d63c445363c5b
Yi Ming <ofseed@foxmail.com>
2022-08-12 19:34:39 +0800

    Update lsp signature config

--- lua/interface/lsp_signature.lua
+++ lua/interface/lsp_signature.lua
@@ -17,6 +17,10 @@ signature.setup {
   -- This setting only take effect in insert mode, it does not affect signature help in normal
   -- mode, 10 by default
 
+  max_height = 12, -- max height of signature floating_window
+  max_width = 80, -- max_width of signature floating_window
+  wrap = true, -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
+
   floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
 
   floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
@@ -24,16 +28,14 @@ signature.setup {
   -- this setting will be helpful if you do not want the PUM and floating win overlap
 
   floating_window_off_x = 0, -- adjust float windows x position.
-  floating_window_off_y = 0, -- adjust float windows y position.
+  floating_window_off_y = 0, -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
 
+  close_timeout = 4000, -- close floating window after ms when laster parameter is entered
   fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
   hint_enable = true, -- virtual hint enable
-  hint_prefix = "➜ ", -- Panda for parameter
+  hint_prefix = "➜ ", -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
   hint_scheme = "String",
   hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
-  max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
-  -- to view the hiding contents
-  max_width = 80, -- max_width of signature floating_window, line will be wrapped if exceed max_width
   handler_opts = {
     border = "rounded", -- double, rounded, single, shadow, none
   },
@@ -51,4 +53,7 @@ signature.setup {
   shadow_guibg = "Black", -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
   timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
   toggle_key = nil, -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+
+  select_signature_key = nil, -- cycle to next signature, e.g. '<M-n>' function overloading
+  move_cursor_key = nil, -- imap, use nvim_set_current_win to move cursor between current win and floating
 }
