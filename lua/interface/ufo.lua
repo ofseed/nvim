99161c6cb42ac80d0587a4b940640898be98f510
Yi Ming <ofseed@foxmail.com>
2022-09-04 18:40:59 +0800

    Add PR link note

--- lua/interface/ufo.lua
+++ lua/interface/ufo.lua
@@ -4,7 +4,8 @@ if not ok then
   return
 end
 
--- //TODO:https://github.com/kevinhwang91/nvim-ufo/issues/4
+-- //TODO: https://github.com/kevinhwang91/nvim-ufo/issues/4
+-- //TODO: https://github.com/neovim/neovim/pull/17446
 -- vim.o.foldcolumn = "1"
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
