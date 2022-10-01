939fc7f36bce962bb624ab96d06e70fec73c2278
Yi Ming <ofseed@foxmail.com>
2022-10-02 00:18:09 +0800

    Update symbols-outline config

--- lua/tool/symbols-outline.lua
+++ lua/tool/symbols-outline.lua
@@ -1,13 +1,25 @@
-vim.g.symbols_outline = {
+local ok, outline = pcall(require, "symbols-outline")
+if not ok then
+  vim.notify "Could not load symbols-outline"
+  return 0
+end
+
+outline.setup {
   highlight_hovered_item = true,
   show_guides = true,
   auto_preview = true,
   position = "right",
+  relative_width = true,
   width = 25,
+  auto_close = false,
   show_numbers = false,
   show_relative_numbers = false,
   show_symbol_details = true,
   preview_bg_highlight = "Pmenu",
+  autofold_depth = nil,
+  auto_unfold_hover = true,
+  fold_markers = { "", "" },
+  wrap = false,
   keymaps = { -- These keymaps can be a string or a table for multiple keys
     close = { "<Esc>", "q" },
     goto_location = "<Cr>",
@@ -16,6 +28,11 @@ vim.g.symbols_outline = {
     toggle_preview = "K",
     rename_symbol = "r",
     code_actions = "a",
+    fold = "h",
+    unfold = "l",
+    fold_all = "W",
+    unfold_all = "E",
+    fold_reset = "R",
   },
   lsp_blacklist = {},
   symbol_blacklist = {},
