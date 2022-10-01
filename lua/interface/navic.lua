fa05035f89fa6045f74ecb60c7461da07b2a5544
Yi Ming <ofseed@foxmail.com>
2022-10-01 23:51:10 +0800

    Enable navic hightlight

--- lua/interface/navic.lua
+++ lua/interface/navic.lua
@@ -34,7 +34,7 @@ navic.setup {
     Operator = " ",
     TypeParameter = " ",
   },
-  highlight = false,
+  highlight = true,
   separator = " > ",
   depth_limit = 0,
   depth_limit_indicator = "..",
