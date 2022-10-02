4ca30a90bf4a3d49d74aa72be19bb5f3dea7cc10
Yi Ming <ofseed@foxmail.com>
2022-10-02 21:36:42 +0800

    Update for neovim v0.8.0

--- lua/edit/copilot.lua
+++ lua/edit/copilot.lua
@@ -2,6 +2,11 @@ vim.g.copilot_filetypes = {
   registers = 0,
 }
 
-vim.keymap.set("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, script = true, expr = true })
-vim.keymap.set("i", "<M-NL>", '<Cmd>Copilot<CR>')
+vim.keymap.set(
+  "i",
+  "<C-J>",
+  'copilot#Accept("<CR>")',
+  { silent = true, script = true, expr = true, replace_keycodes = false }
+)
+vim.keymap.set("i", "<M-NL>", "<Cmd>Copilot<CR>")
 vim.g.copilot_no_tab_map = true
