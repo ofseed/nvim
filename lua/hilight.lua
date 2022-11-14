e50b81adb85fd20e1e21981c5ea5251ff57217f3
Yi Ming <ofseed@foxmail.com>
2022-11-14 13:50:45 +0800

    Set hilights with lua style

--- lua/hilight.lua
+++ lua/hilight.lua
@@ -1,12 +1,10 @@
 vim.api.nvim_create_autocmd("ColorScheme", {
   pattern = "*",
   callback = function()
-    vim.cmd [[
-hi link GitSignsChangeLn DiffText
-hi link GitSignsAddInline GitSignsAddLn
-hi link GitSignsDeleteInline GitSignsDeleteLn
-hi link GitSignsChangeInline GitSignsChangeLn
-hi link GitSignsDeleteLn DiffDelete
-]]
+    vim.api.nvim_set_hl(0, "GitSignsChangeLn", { link = "DiffText" })
+    vim.api.nvim_set_hl(0, "GitSignsAddInline", { link = "GitSignsAddLn" })
+    vim.api.nvim_set_hl(0, "GitSignsDeleteInline", { link = "GitSignsDeleteLn" })
+    vim.api.nvim_set_hl(0, "GitSignsChangeInline", { link = "GitSignsChangeLn" })
+    vim.api.nvim_set_hl(0, "GitSignsDeleteLn", { link = "DiffDelete" })
   end,
 })
