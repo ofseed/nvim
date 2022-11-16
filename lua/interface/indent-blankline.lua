53d52852ee3a174cf181a6c6f45054d774b3975c
Yi Ming <ofseed@foxmail.com>
2022-11-16 12:15:39 +0800

    Enable show current context start

--- lua/interface/indent-blankline.lua
+++ lua/interface/indent-blankline.lua
@@ -5,7 +5,7 @@ end
 
 blankline.setup {
   show_current_context = true,
-  -- show_current_context_start = true,
+  show_current_context_start = true,
   -- use_treesitter = true,
   -- use_treesitter_scope = true,
 }
