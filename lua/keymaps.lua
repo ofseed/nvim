567a34869111e32fe4f531960e74df2bfc70bc6a
Yi Ming <ofseed@foxmail.com>
2022-11-23 16:16:45 +0800

    Remove unused keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,5 @@
 -- Keybindings are only used for lazy loaded plugins
 vim.keymap.set("n", "<leader>a", "<Cmd>AerialToggle<CR>", { desc = "Outline" })
-vim.keymap.set("n", "<leader>to", "<Cmd>SymbolsOutline<CR>", { desc = "Outline" })
 vim.keymap.set("n", "<leader>tz", "<Cmd>ZenMode<CR>", { desc = "Zen Mode" })
 vim.keymap.set("n", "<leader>gg", "<Cmd>Neogit<CR>", { desc = "Open Neogit" })
 vim.keymap.set("n", "<leader>gdo", "<Cmd>DiffviewOpen<CR>", { desc = "Open" })
