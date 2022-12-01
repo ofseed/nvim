0c89469efd056e8646be555f368df84583391147
Yi Ming <ofseed@foxmail.com>
2022-12-01 17:04:45 +0800

    Update noice config

--- lua/interface/noice.lua
+++ lua/interface/noice.lua
@@ -18,6 +18,9 @@ noice.setup {
       cmdline = { pattern = "^:", icon = " :", lang = "vim" },
       search_down = { kind = "search", pattern = "^/", icon = " /", lang = "regex" },
       search_up = { kind = "search", pattern = "^%?", icon = " ?", lang = "regex" },
+      filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+      lua = { pattern = "^:%s*lua%s+", icon = "", lang = "lua" },
+      help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
       input = {}, -- Used by input()
       -- lua = false, -- to disable a format, set to `false`
     },
@@ -38,19 +41,41 @@ noice.setup {
     -- Icons for completion item kinds (see defaults at noice.config.icons.kinds)
     kind_icons = {}, -- set to `false` to disable icons
   },
+  -- default options for require('noice').redirect
+  -- see the section on Command Redirection
+  redirect = {
+    view = "popup",
+    filter = { event = "msg_show" },
+  },
   -- You can add any custom commands below that will be available with `:Noice command`
   commands = {
     history = {
       -- options for the message history that you get with `:Noice`
       view = "split",
       opts = { enter = true, format = "details" },
-      filter = { event = { "msg_show", "notify" }, ["not"] = { kind = { "search_count", "echo" } } },
+      filter = {
+        any = {
+          { event = "notify" },
+          { error = true },
+          { warning = true },
+          { event = "msg_show", kind = { "" } },
+          { event = "lsp", kind = "message" },
+        },
+      },
     },
     -- :Noice last
     last = {
       view = "popup",
       opts = { enter = true, format = "details" },
-      filter = { event = { "msg_show", "notify" }, ["not"] = { kind = { "search_count", "echo" } } },
+      filter = {
+        any = {
+          { event = "notify" },
+          { error = true },
+          { warning = true },
+          { event = "msg_show", kind = { "" } },
+          { event = "lsp", kind = "message" },
+        },
+      },
       filter_opts = { count = 1 },
     },
     -- :Noice errors
@@ -81,6 +106,14 @@ noice.setup {
       throttle = 1000 / 30, -- frequency to update lsp progress message
       view = "mini",
     },
+    override = {
+      -- override the default lsp markdown formatter with Noice
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+      -- override the lsp markdown formatter with Noice
+      ["vim.lsp.util.stylize_markdown"] = false,
+      -- override cmp documentation with Noice (needs the other options to work)
+      ["cmp.entry.get_documentation"] = false,
+    },
     hover = {
       enabled = false,
       view = nil, -- when nil, use defaults from documentation
@@ -88,9 +121,20 @@ noice.setup {
     },
     signature = {
       enabled = false,
-      auto_open = true, -- Automatically show signature help when typing a trigger character from the LSP
+      auto_open = {
+        enabled = true,
+        trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+        luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+        throttle = 50, -- Debounce lsp signature help request by 50ms
+      },
       view = nil, -- when nil, use defaults from documentation
       opts = {}, -- merged with defaults from documentation
+    },
+    message = {
+      -- Messages shown by lsp servers
+      enabled = true,
+      view = "notify",
+      opts = {},
     },
     -- defaults for hover and signature help
     documentation = {
@@ -134,6 +178,7 @@ noice.setup {
     command_palette = false, -- position the cmdline and popupmenu together
     long_message_to_split = false, -- long messages will be sent to a split
     inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
   },
   throttle = 1000 / 30, -- how frequently does Noice need to check for ui updates? This has no effect when in blocking mode.
   views = {}, ---@see section on views
