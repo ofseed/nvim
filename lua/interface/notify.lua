6bd5da9209e4d171f34acb33566432dc454168dc
Yi Ming <ofseed@foxmail.com>
2022-12-02 19:33:54 +0800

    Update and initial notify config

--- lua/interface/notify.lua
+++ lua/interface/notify.lua
@@ -5,36 +5,21 @@ if not ok then
 end
 
 notify.setup {
-  -- Animation style (see below for details)
-  stages = "fade",
-
-  -- Function called when a new window is opened, use for changing win settings/config
-  on_open = nil,
-
-  -- Function called when a window is closed
-  on_close = nil,
-
-  -- Render function for notifications. See notify-render()
-  render = "default",
-
-  -- Default timeout for notifications
-  timeout = 1000,
-
-  -- For stages that change opacity this is treated as the highlight behind the window
-  -- Set this to either a highlight group or an RGB hex value e.g. "#000000"
   background_colour = "Normal",
-
-  -- Minimum width for notification windows
-  minimum_width = 50,
-
-  -- Icons for the different levels
+  fps = 30,
   icons = {
-    ERROR = "",
-    WARN = "",
-    INFO = "",
     DEBUG = "",
+    ERROR = "",
+    INFO = "",
     TRACE = "✎",
+    WARN = "",
   },
+  level = 2,
+  minimum_width = 50,
+  render = "default",
+  stages = "fade_in_slide_out",
+  timeout = 5000,
+  top_down = true,
 }
 
 vim.notify = notify
