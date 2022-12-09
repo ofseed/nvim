36509f3354e54fa757e1265d347ea5eeb23df977
Yi Ming <ofseed@foxmail.com>
2022-12-09 20:38:00 +0800

    Fix super tab snippets jump behavior

--- lua/edit/cmp.lua
+++ lua/edit/cmp.lua
@@ -50,7 +50,7 @@ cmp.setup {
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
+      elseif luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
       elseif has_words_before() then
         cmp.complete()
