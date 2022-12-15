0f0284b4d704459ca71969ef70b38edf45bd5c75
Yi Ming <ofseed@foxmail.com>
2022-12-15 22:12:02 +0800

    Update illuminate config

--- lua/interface/illuminate.lua
+++ lua/interface/illuminate.lua
@@ -12,6 +12,10 @@ illuminate.configure {
   },
   -- delay: delay in milliseconds
   delay = 100,
+  -- filetype_overrides: filetype specific overrides.
+  -- The keys are strings to represent the filetype while the values are tables that
+  -- supports the same keys passed to .configure except for filetypes_denylist and filetypes_allowlist
+  filetype_overrides = {},
   -- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
   filetypes_denylist = {
     "dirvish",
@@ -29,8 +33,10 @@ illuminate.configure {
   -- filetypes_allowlist: filetypes to illuminate, this is overriden by filetypes_denylist
   filetypes_allowlist = {},
   -- modes_denylist: modes to not illuminate, this overrides modes_allowlist
+  -- See `:help mode()` for possible values
   modes_denylist = {},
   -- modes_allowlist: modes to illuminate, this is overriden by modes_denylist
+  -- See `:help mode()` for possible values
   modes_allowlist = {},
   -- providers_regex_syntax_denylist: syntax to not illuminate, this overrides providers_regex_syntax_allowlist
   -- Only applies to the 'regex' provider
@@ -42,6 +48,15 @@ illuminate.configure {
   providers_regex_syntax_allowlist = {},
   -- under_cursor: whether or not to illuminate under the cursor
   under_cursor = true,
+  -- large_file_cutoff: number of lines at which to use large_file_config
+  -- The `under_cursor` option is disabled when this cutoff is hit
+  large_file_cutoff = nil,
+  -- large_file_config: config to use for large files (based on large_file_cutoff).
+  -- Supports the same keys passed to .configure
+  -- If nil, vim-illuminate will be disabled for large files.
+  large_file_overrides = nil,
+  -- min_count_to_highlight: minimum number of matches required to perform highlighting
+  min_count_to_highlight = 1,
 }
 
 vim.keymap.set("n", "<M-n>", illuminate.goto_next_reference, { desc = "Next reference" })
