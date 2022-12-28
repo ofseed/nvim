a41018efc15451b64cd28750a651103f2a495eb3
Yi Ming <ofseed@foxmail.com>
2022-12-29 02:35:37 +0800

    Do not show help hint in diffview

--- lua/tool/diffview.lua
+++ lua/tool/diffview.lua
@@ -11,7 +11,7 @@ diffview.setup {
   enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
   git_cmd = { "git" }, -- The git executable followed by default args.
   use_icons = true, -- Requires nvim-web-devicons
-  show_help_hints = true, -- Show hints for how to open the help panel
+  show_help_hints = false, -- Show hints for how to open the help panel
   watch_index = true, -- Update views and index buffers when the git index changes.
   icons = { -- Only applies when use_icons is true.
     folder_closed = "",
