44a9dda1016a5f1bbaa083b30c21f3af03906e6c
Yi Ming <ofseed@foxmail.com>
2023-01-25 17:58:12 +0800

    Do not show extmark for luasnip anymore

--- lua/edit/snip.lua
+++ lua/edit/snip.lua
@@ -7,16 +7,16 @@ end
 local types = require "luasnip.util.types"
 
 snip.config.setup {
-  ext_opts = {
-    [types.choiceNode] = {
-      active = {
-        virt_text = { { "●", "DiagnosticWarn" } },
-      },
-    },
-    [types.insertNode] = {
-      active = {
-        virt_text = { { "●", "DiagnosticInfo" } },
-      },
-    },
-  },
+  -- ext_opts = {
+  --   [types.choiceNode] = {
+  --     active = {
+  --       virt_text = { { "●", "DiagnosticWarn" } },
+  --     },
+  --   },
+  --   [types.insertNode] = {
+  --     active = {
+  --       virt_text = { { "●", "DiagnosticInfo" } },
+  --     },
+  --   },
+  -- },
 }
