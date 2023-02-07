c9021e2903b2a08fe5f273464d13f54349e0c7a3
Yi Ming <ofseed@foxmail.com>
2023-02-07 22:28:21 +0800

    Update dapui config

--- lua/debug/dap-ui.lua
+++ lua/debug/dap-ui.lua
@@ -5,74 +5,83 @@ if not ok then
 end
 
 dapui.setup {
-  icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
-  mappings = {
-    -- Use a table to apply multiple mappings
-    expand = { "<CR>", "<2-LeftMouse>" },
-    open = "o",
-    remove = "d",
-    edit = "e",
-    repl = "r",
-    toggle = "t",
-  },
-  -- Expand lines larger than the window
-  -- Requires >= 0.7
-  expand_lines = vim.fn.has "nvim-0.7",
-  -- Layouts define sections of the screen to place windows.
-  -- The position can be "left", "right", "top" or "bottom".
-  -- The size specifies the height/width depending on position. It can be an Int
-  -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
-  -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
-  -- Elements are the elements shown in the layout (in order).
-  -- Layouts are opened in order so that earlier layouts take priority in window sizing.
-  layouts = {
-    {
-      elements = {
-        -- Elements can be strings or table with id and size keys.
-        { id = "scopes", size = 0.25 },
-        "breakpoints",
-        "stacks",
-        "watches",
-      },
-      size = 40, -- 40 columns
-      position = "left",
-    },
-    {
-      elements = {
-        "repl",
-        "console",
-      },
-      size = 0.25, -- 25% of total lines
-      position = "bottom",
-    },
-  },
   controls = {
-    enabled = true,
-    -- Display controls in this element
     element = "repl",
+    enabled = true,
     icons = {
-      pause = "",
-      play = "",
-      step_into = "",
-      step_over = "",
-      step_out = "",
-      step_back = "",
-      run_last = "↻",
-      terminate = "□",
+      pause = "",
+      play = "",
+      run_last = "",
+      step_back = "",
+      step_into = "",
+      step_out = "",
+      step_over = "",
+      terminate = "",
     },
   },
+  element_mappings = {},
+  expand_lines = true,
   floating = {
-    max_height = nil, -- These can be integers or a float between 0 and 1.
-    max_width = nil, -- Floats will be treated as percentage of your screen.
-    border = "single", -- Border style. Can be "single", "double" or "rounded"
+    border = "single",
     mappings = {
       close = { "q", "<Esc>" },
     },
   },
-  windows = { indent = 1 },
+  force_buffers = true,
+  icons = {
+    collapsed = "",
+    current_frame = "",
+    expanded = "",
+  },
+  layouts = {
+    {
+      elements = {
+        {
+          id = "scopes",
+          size = 0.25,
+        },
+        {
+          id = "breakpoints",
+          size = 0.25,
+        },
+        {
+          id = "stacks",
+          size = 0.25,
+        },
+        {
+          id = "watches",
+          size = 0.25,
+        },
+      },
+      position = "left",
+      size = 40,
+    },
+    {
+      elements = {
+        {
+          id = "repl",
+          size = 0.5,
+        },
+        {
+          id = "console",
+          size = 0.5,
+        },
+      },
+      position = "bottom",
+      size = 10,
+    },
+  },
+  mappings = {
+    edit = "e",
+    expand = { "<CR>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    repl = "r",
+    toggle = "t",
+  },
   render = {
-    max_type_length = nil, -- Can be integer or nil.
-    max_value_lines = 100, -- Can be integer or nil.
+    indent = 1,
+    max_value_lines = 100,
   },
 }
 
