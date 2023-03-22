046872bc19813830eb5db9794fdbcaa8f66a4a35
Yi Ming <ofseed@foxmail.com>
2023-03-22 12:34:22 +0800

    Remove unneeded keys since it is not stable

--- lua/interface/statuscol.lua
+++ lua/interface/statuscol.lua
@@ -6,13 +6,6 @@ end
 local builtin = require "statuscol.builtin"
 
 statuscol.setup {
-  -- Builtin line number string options for ScLn() segment
-  thousands = false, -- or line number thousands separator string ("." / ",")
-  relculright = false, -- whether to right-align the cursor line number with 'relativenumber' set
-  -- Builtin 'statuscolumn' options
-  setopt = true, -- whether to set the 'statuscolumn', providing builtin click actions
-  ft_ignore = nil, -- lua table with filetypes for which 'statuscolumn' will be unset
-  -- Default segments (fold -> sign -> line number + separator)
   segments = {
     { text = { "%C" }, click = "v:lua.ScFa" },
     {
@@ -28,23 +21,5 @@ statuscol.setup {
       sign = { name = { "GitSigns" }, maxwidth = 1, colwidth = 1 },
       click = "v:lua.ScSa",
     },
-  },
-  clickhandlers = {
-    Lnum = builtin.lnum_click,
-    FoldClose = builtin.foldclose_click,
-    FoldOpen = builtin.foldopen_click,
-    FoldOther = builtin.foldother_click,
-    DapBreakpointRejected = builtin.toggle_breakpoint,
-    DapBreakpoint = builtin.toggle_breakpoint,
-    DapBreakpointCondition = builtin.toggle_breakpoint,
-    DiagnosticSignError = builtin.diagnostic_click,
-    DiagnosticSignHint = builtin.diagnostic_click,
-    DiagnosticSignInfo = builtin.diagnostic_click,
-    DiagnosticSignWarn = builtin.diagnostic_click,
-    GitSignsTopdelete = builtin.gitsigns_click,
-    GitSignsUntracked = builtin.gitsigns_click,
-    GitSignsAdd = builtin.gitsigns_click,
-    GitSignsChangedelete = builtin.gitsigns_click,
-    GitSignsDelete = builtin.gitsigns_click,
   },
 }
