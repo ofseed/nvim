ce3facf487f60c00b601fbba49a33ab27b0a92ee
Yi Ming <ofseed@foxmail.com>
2023-04-09 13:41:27 +0800

    Update mason-null-ls config

--- lua/core/null-ls.lua
+++ lua/core/null-ls.lua
@@ -8,16 +8,7 @@ local os, mason = pcall(require, "mason-null-ls")
 
 mason.setup {
   automatic_setup = true,
-}
-
-mason.setup_handlers {
-  function(source_name, methods)
-    -- all sources with no handler get passed here
-
-    -- To keep the original functionality of `automatic_setup = true`,
-    -- please add the below.
-    require "mason-null-ls.automatic_setup"(source_name, methods)
-  end,
+  handlers = {},
 }
 
 ls.setup {
