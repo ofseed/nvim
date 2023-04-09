8a346369670c0145a83eca02b67c5de906d1c28b
Yi Ming <ofseed@foxmail.com>
2023-04-09 15:55:35 +0800

    Remove redundant treesitter config in orgmode

--- lua/tool/orgmode.lua
+++ lua/tool/orgmode.lua
@@ -4,23 +4,8 @@ if not ok then
   return
 end
 
-local treesitter_config = require "nvim-treesitter.configs"
-
 -- Load custom treesitter grammar for org filetype
 orgmode.setup_ts_grammar()
-
--- Treesitter configuration
-require("nvim-treesitter.configs").setup {
-  -- If TS highlights are not enabled at all, or disabled via `disable` prop,
-  -- highlighting will fallback to default Vim syntax highlighting
-  highlight = {
-    enable = true,
-    -- Required for spellcheck, some LaTex highlights and
-    -- code block highlights that do not have ts grammar
-    additional_vim_regex_highlighting = { "org" },
-  },
-  ensure_installed = { "org" }, -- Or run :TSUpdate org
-}
 
 orgmode.setup {
   org_agenda_files = { "~/Documents/org/*" },
