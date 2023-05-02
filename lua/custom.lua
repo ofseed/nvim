549b83cc8762db11d468669f7b52fb1021df829a
Yi Ming <ofseed@foxmail.com>
2023-05-02 21:28:54 +0800

    Define custom icons

---
+++ lua/custom.lua
@@ -0,0 +1,42 @@
+local M = {}
+
+M.icons = {
+  kind = {
+    Array = "",
+    Boolean = "",
+    Class = "",
+    Color = "",
+    Constant = "",
+    Constructor = "",
+    Enum = "",
+    EnumMember = "",
+    Event = "",
+    Field = "",
+    File = "",
+    Folder = "",
+    Function = "",
+    Interface = "",
+    Key = "",
+    Keyword = "",
+    Method = "",
+    Module = "",
+    Namespace = "",
+    Null = "",
+    Number = "",
+    Object = "",
+    Operator = "",
+    Package = "",
+    Property = "",
+    Reference = "",
+    Snippet = "",
+    String = "",
+    Struct = "",
+    Text = "",
+    TypeParameter = "",
+    Unit = "",
+    Value = "",
+    Variable = "",
+  },
+}
+
+return M
