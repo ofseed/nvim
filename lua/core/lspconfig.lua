798d24cae669e59423dc99259cd69d2822775885
Yi Ming <ofseed@foxmail.com>
2023-05-02 20:25:28 +0800

    Update LSP diagnostic icons

--- lua/core/lspconfig.lua
+++ lua/core/lspconfig.lua
@@ -25,7 +25,7 @@ vim.diagnostic.config {
 }
 
 -- Set diagnostic icons
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
