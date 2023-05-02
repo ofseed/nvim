12897cdffa27a08428e8d6d717642d8332d91c82
Yi Ming <ofseed@foxmail.com>
2023-05-02 22:17:31 +0800

    Update bufferline buffer close icon

--- lua/interface/bufferline.lua
+++ lua/interface/bufferline.lua
@@ -18,7 +18,7 @@ bufferline.setup {
       icon = "▎", -- this should be omitted if indicator style is not 'icon'
       style = "icon", -- "icon" | "underline" | "none",
     },
-    buffer_close_icon = "",
+    buffer_close_icon = "",
     modified_icon = "●",
     close_icon = "",
     left_trunc_marker = "",
