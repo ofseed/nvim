e2f3ed30ac950ac5f078bb703138dc4361aa91a2
Yi Ming <ofseed@foxmail.com>
2023-05-22 15:12:53 +0800

    Update navbuddy config

--- lua/tool/navbuddy.lua
+++ lua/tool/navbuddy.lua
@@ -33,6 +33,14 @@ navbuddy.setup {
       },
     },
   },
+  node_markers = {
+    enabled = true,
+    icons = {
+      leaf = "  ",
+      leaf_selected = " → ",
+      branch = " ",
+    },
+  },
   icons = {
     Array = " ",
     Boolean = " ",
@@ -74,42 +82,54 @@ navbuddy.setup {
   -- mappings are used for keys
   -- that are not set by user
   mappings = {
-    ["<esc>"] = actions.close, -- Close and cursor to original location
-    ["q"] = actions.close,
+    ["<esc>"] = actions.close(), -- Close and cursor to original location
+    ["q"] = actions.close(),
 
-    ["j"] = actions.next_sibling, -- down
-    ["k"] = actions.previous_sibling, -- up
+    ["j"] = actions.next_sibling(), -- down
+    ["k"] = actions.previous_sibling(), -- up
 
-    ["h"] = actions.parent, -- Move to left panel
-    ["l"] = actions.children, -- Move to right panel
-    ["0"] = actions.root, -- Move to first panel
+    ["h"] = actions.parent(), -- Move to left panel
+    ["l"] = actions.children(), -- Move to right panel
+    ["0"] = actions.root(), -- Move to first panel
 
-    ["v"] = actions.visual_name, -- Visual selection of name
-    ["V"] = actions.visual_scope, -- Visual selection of scope
+    ["v"] = actions.visual_name(), -- Visual selection of name
+    ["V"] = actions.visual_scope(), -- Visual selection of scope
 
-    ["y"] = actions.yank_name, -- Yank the name to system clipboard "+
-    ["Y"] = actions.yank_scope, -- Yank the scope to system clipboard "+
+    ["y"] = actions.yank_name(), -- Yank the name to system clipboard "+
+    ["Y"] = actions.yank_scope(), -- Yank the scope to system clipboard "+
 
-    ["i"] = actions.insert_name, -- Insert at start of name
-    ["I"] = actions.insert_scope, -- Insert at start of scope
+    ["i"] = actions.insert_name(), -- Insert at start of name
+    ["I"] = actions.insert_scope(), -- Insert at start of scope
 
-    ["a"] = actions.append_name, -- Insert at end of name
-    ["A"] = actions.append_scope, -- Insert at end of scope
+    ["a"] = actions.append_name(), -- Insert at end of name
+    ["A"] = actions.append_scope(), -- Insert at end of scope
 
-    ["r"] = actions.rename, -- Rename currently focused symbol
+    ["r"] = actions.rename(), -- Rename currently focused symbol
 
-    ["d"] = actions.delete, -- Delete scope
+    ["d"] = actions.delete(), -- Delete scope
 
-    ["f"] = actions.fold_create, -- Create fold of current scope
-    ["F"] = actions.fold_delete, -- Delete fold of current scope
+    ["f"] = actions.fold_create(), -- Create fold of current scope
+    ["F"] = actions.fold_delete(), -- Delete fold of current scope
 
-    ["c"] = actions.comment, -- Comment out current scope
+    ["c"] = actions.comment(), -- Comment out current scope
 
-    ["<enter>"] = actions.select, -- Goto selected symbol
-    ["o"] = actions.select,
+    ["<enter>"] = actions.select(), -- Goto selected symbol
+    ["o"] = actions.select(),
 
-    ["J"] = actions.move_down, -- Move focused node down
-    ["K"] = actions.move_up, -- Move focused node up
+    ["J"] = actions.move_down(), -- Move focused node down
+    ["K"] = actions.move_up(), -- Move focused node up
+
+    ["t"] = actions.telescope { -- Fuzzy finder at current level.
+      layout_config = { -- All options that can be
+        height = 0.60, -- passed to telescope.nvim's
+        width = 0.60, -- default can be passed here.
+        prompt_position = "top",
+        preview_width = 0.50,
+      },
+      layout_strategy = "horizontal",
+    },
+
+    ["g?"] = actions.help(), -- Open mappings help window
   },
   lsp = {
     auto_attach = true, -- If set to true, you don't need to manually use attach function
