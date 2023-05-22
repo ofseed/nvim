7be05738effcfffaf547d734c4661b055fd6776c
Yi Ming <ofseed@foxmail.com>
2023-05-22 16:12:05 +0800

    Enable fold column

--- lua/interface/ufo.lua
+++ lua/interface/ufo.lua
@@ -6,7 +6,7 @@ end
 
 -- //TODO: https://github.com/kevinhwang91/nvim-ufo/issues/4
 -- //TODO: https://github.com/neovim/neovim/pull/17446
--- vim.o.foldcolumn = "1"
+vim.o.foldcolumn = "1"
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
