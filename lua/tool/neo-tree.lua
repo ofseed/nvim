055592473628b0ab9c04f846b0043d5d6e0daeee
Yi Ming <ofseed@foxmail.com>
2023-06-20 11:33:03 +0800

    Configure neo-tree

--- lua/tool/neo-tree.lua
+++ lua/tool/neo-tree.lua
@@ -6,6 +6,45 @@ end
 
 local custom = require "custom"
 
-tree.setup {}
+tree.setup {
+  source_selector = {
+    winbar = true,
+    sources = {
+      {
+        source = "filesystem",
+        display_name = "  Files ",
+      },
+      {
+        source = "buffers",
+        display_name = "  Buffers ",
+      },
+      {
+        source = "git_status",
+        display_name = "  Git ",
+      },
+    },
+  },
+  default_component_configs = {
+    container = {
+      enable_character_fade = false,
+    },
+    git_status = {
+      symbols = {
+        added = "",
+        modified = "",
+        deleted = "",
+        renamed = "",
+        untracked = "",
+        ignored = "",
+        unstaged = "",
+        staged = "",
+        conflict = "",
+      },
+    },
+  },
+  window = {
+    width = custom.width,
+  },
+}
 
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "File Explorer" })
