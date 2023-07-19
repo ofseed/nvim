35d584ba73f1fa37af00a0122aaf6f4ad6139f70
Yi Ming <ofseed@foxmail.com>
2023-07-19 14:13:51 +0800

    Reconfigure navbuddy

--- lua/tool/navbuddy.lua
+++ lua/tool/navbuddy.lua
@@ -1,11 +1,6 @@
-local config = function ()
-local navbuddy = require "nvim-navbuddy"
-
-local actions = require "nvim-navbuddy.actions"
-
 local custom = require "custom"
 
-navbuddy.setup {
+local opts = {
   window = {
     border = custom.border, -- "rounded", "double", "solid", "none"
     -- or an array with eight chars building up the border in a clockwise fashion
@@ -79,56 +74,6 @@ navbuddy.setup {
   -- by user are set. Else default
   -- mappings are used for keys
   -- that are not set by user
-  mappings = {
-    ["<esc>"] = actions.close(), -- Close and cursor to original location
-    ["q"] = actions.close(),
-
-    ["j"] = actions.next_sibling(), -- down
-    ["k"] = actions.previous_sibling(), -- up
-
-    ["h"] = actions.parent(), -- Move to left panel
-    ["l"] = actions.children(), -- Move to right panel
-    ["0"] = actions.root(), -- Move to first panel
-
-    ["v"] = actions.visual_name(), -- Visual selection of name
-    ["V"] = actions.visual_scope(), -- Visual selection of scope
-
-    ["y"] = actions.yank_name(), -- Yank the name to system clipboard "+
-    ["Y"] = actions.yank_scope(), -- Yank the scope to system clipboard "+
-
-    ["i"] = actions.insert_name(), -- Insert at start of name
-    ["I"] = actions.insert_scope(), -- Insert at start of scope
-
-    ["a"] = actions.append_name(), -- Insert at end of name
-    ["A"] = actions.append_scope(), -- Insert at end of scope
-
-    ["r"] = actions.rename(), -- Rename currently focused symbol
-
-    ["d"] = actions.delete(), -- Delete scope
-
-    ["f"] = actions.fold_create(), -- Create fold of current scope
-    ["F"] = actions.fold_delete(), -- Delete fold of current scope
-
-    ["c"] = actions.comment(), -- Comment out current scope
-
-    ["<enter>"] = actions.select(), -- Goto selected symbol
-    ["o"] = actions.select(),
-
-    ["J"] = actions.move_down(), -- Move focused node down
-    ["K"] = actions.move_up(), -- Move focused node up
-
-    ["t"] = actions.telescope { -- Fuzzy finder at current level.
-      layout_config = { -- All options that can be
-        height = 0.60, -- passed to telescope.nvim's
-        width = 0.60, -- default can be passed here.
-        prompt_position = "top",
-        preview_width = 0.50,
-      },
-      layout_strategy = "horizontal",
-    },
-
-    ["g?"] = actions.help(), -- Open mappings help window
-  },
   lsp = {
     auto_attach = true, -- If set to true, you don't need to manually use attach function
     preference = nil, -- list of lsp server names in order of preference
@@ -141,17 +86,21 @@ navbuddy.setup {
   },
 }
 
-vim.keymap.set("n", "<leader>n", navbuddy.open, { desc = "Navigator" })
-
-end
-
 return {
   "SmiteshP/nvim-navbuddy",
-  event = "VeryLazy",
   dependencies = {
     "neovim/nvim-lspconfig",
     "SmiteshP/nvim-navic",
     "MunifTanjim/nui.nvim",
   },
-  config = config,
+  opts = opts,
+  keys = {
+    {
+      "<leader>n",
+      function()
+        require("nvim-navbuddy").open()
+      end,
+      { desc = "Navigator" },
+    },
+  },
 }
