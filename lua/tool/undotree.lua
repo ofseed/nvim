836cb37ab6a4683e1cfab3fd2e555a2600a6bd46
Yi Ming <ofseed@foxmail.com>
2023-07-21 23:19:51 +0800

    Reconfigure undotree

--- lua/tool/undotree.lua
+++ lua/tool/undotree.lua
@@ -1,6 +1,14 @@
 return {
   "mbbill/undotree",
   config = function()
+    local custom = require "custom"
+    local width = custom.width()
+
+    vim.g.undotree_SplitWidth = width
+    vim.g.undotree_WindowLayout = 2
+    vim.g.undotree_DiffAutoOpen = 0
+    vim.g.undotree_SetFocusWhenToggle = 1
+    vim.g.undotree_ShortIndicators = 1
     vim.g.undotree_HelpLine = 0
   end,
   keys = {
