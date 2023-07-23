a7dcc4036f3ba998bae752e5fb6ac9f2f0ccb92f
Yi Ming <ofseed@foxmail.com>
2023-07-23 21:13:51 +0800

    Disable formatting ability providing by gopls

--- lua/lsp/go.lua
+++ lua/lsp/go.lua
@@ -48,7 +48,7 @@ local opts = {
   lsp_diag_virtual_text = { space = 0, prefix = "" },
   lsp_diag_signs = true,
   lsp_diag_update_in_insert = false,
-  lsp_document_formatting = true,
+  lsp_document_formatting = false,
   -- set to true: use gopls to format
   -- false if you want to use other formatter tool(e.g. efm, nulls)
   lsp_inlay_hints = {
