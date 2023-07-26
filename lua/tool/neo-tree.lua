2422d024648cb5c79b67d72b49c128a21e0bdd54
Yi Ming <ofseed@foxmail.com>
2023-07-26 17:22:10 +0800

    Add `h` and `l` to navigate

--- lua/tool/neo-tree.lua
+++ lua/tool/neo-tree.lua
@@ -32,6 +32,24 @@ return {
       width = custom.width,
       mappings = {
         ["<Space>"] = "none",
+        ["h"] = function(state)
+          local node = state.tree:get_node()
+          if node.type == "directory" and node:is_expanded() then
+            require("neo-tree.sources.filesystem").toggle_directory(state, node)
+          else
+            require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+          end
+        end,
+        ["l"] = function(state)
+          local node = state.tree:get_node()
+          if node.type == "directory" then
+            if not node:is_expanded() then
+              require("neo-tree.sources.filesystem").toggle_directory(state, node)
+            elseif node:has_children() then
+              require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+            end
+          end
+        end,
       },
     },
     filesystem = {
