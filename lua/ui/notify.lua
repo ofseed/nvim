0630a0610fe5530c31095a312bae7eb96c775db4
Yi Ming <ofseed@foxmail.com>
2023-07-29 22:15:36 +0800

    Reconfigure notify

--- lua/ui/notify.lua
+++ lua/ui/notify.lua
@@ -1,27 +1,13 @@
-local opts = {
-  background_colour = "Normal",
-  fps = 30,
-  icons = {
-    DEBUG = "",
-    ERROR = "",
-    INFO = "",
-    TRACE = "✎",
-    WARN = "",
-  },
-  level = 2,
-  minimum_width = 50,
-  render = "default",
-  stages = "static",
-  timeout = 2500,
-  top_down = true,
-}
-
 return {
   "rcarriga/nvim-notify",
   event = "VeryLazy",
   config = function()
     local notify = require "notify"
-    notify.setup(opts)
+    notify.setup {
+      render = "compact",
+      stages = "static",
+      timeout = 2500,
+    }
     vim.notify = notify
   end,
 }
