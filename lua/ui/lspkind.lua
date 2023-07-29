bc62c0c6963ce5b3ebe2ad716467dc5793096ef7
Yi Ming <ofseed@foxmail.com>
2023-07-29 23:54:25 +0800

    Reconfigure lspkind.nvim

--- lua/ui/lspkind.lua
+++ lua/ui/lspkind.lua
@@ -1,32 +1,11 @@
 local custom = require "custom"
 
-local opts = {
-  -- DEPRECATED (use mode instead): enables text annotations
-  --
-  -- default: true
-  -- with_text = true,
-
-  -- defines how annotations are shown
-  -- default: symbol
-  -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
-  mode = "symbol_text",
-
-  -- default symbol map
-  -- can be either 'default' (requires nerd-fonts font) or
-  -- 'codicons' for codicon preset (requires vscode-codicons font)
-  --
-  -- default: 'default'
-  preset = "codicons",
-
-  -- override preset symbols
-  --
-  -- default: {}
-  symbol_map = custom.icons.kind,
-}
 return {
-  "onsails/lspkind-nvim",
-  event = "VeryLazy",
+  "onsails/lspkind.nvim",
+  lazy = true,
   config = function()
-    require("lspkind").init(opts)
+    require("lspkind").init {
+      symbol_map = custom.icons.kind,
+    }
   end,
 }
