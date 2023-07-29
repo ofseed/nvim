559072e81fe05ac068cc025147d53de38f158f47
Yi Ming <ofseed@foxmail.com>
2023-07-29 21:21:21 +0800

    Reconfigure rose-pine

--- lua/colorscheme/rose-pine.lua
+++ lua/colorscheme/rose-pine.lua
@@ -1,47 +1,10 @@
-local opts = {
-  --- @usage 'main' | 'moon'
-  dark_variant = "main",
-  bold_vert_split = false,
-  dim_nc_background = false,
-  disable_background = false,
-  disable_float_background = false,
-  disable_italics = false,
-
-  --- @usage string hex value or named color from rosepinetheme.com/palette
-  groups = {
-    background = "base",
-    panel = "surface",
-    border = "highlight_med",
-    comment = "muted",
-    link = "iris",
-    punctuation = "subtle",
-
-    error = "love",
-    hint = "iris",
-    info = "foam",
-    warn = "gold",
-
-    headings = {
-      h1 = "iris",
-      h2 = "foam",
-      h3 = "rose",
-      h4 = "gold",
-      h5 = "pine",
-      h6 = "foam",
-    },
-    -- or set all headings at once
-    -- headings = 'subtle'
-  },
-
-  -- Change specific vim highlight groups
-  highlight_groups = {
-    ColorColumn = { bg = "rose" },
-  },
-}
-
 return {
   "rose-pine/neovim",
   lazy = true,
   name = "rose-pine",
-  opts = opts,
+  opts = {
+    highlight_groups = {
+      EndOfBuffer = { fg = "base" },
+    },
+  },
 }
