7985cd9924569be8c51278b6df640c5efd6c5b93
Yi Ming <ofseed@foxmail.com>
2023-07-30 15:44:08 +0800

    Use undercurl in nvim-lsp

--- lua/colorscheme/catppuccin.lua
+++ lua/colorscheme/catppuccin.lua
@@ -10,6 +10,14 @@ return {
       markdown = true,
       mason = true,
       neotree = true,
+      native_lsp = {
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
       navic = {
         enabled = true,
       },
