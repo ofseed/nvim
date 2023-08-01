eb12c8deeb7b4c48be07c742401da2b082043a1c
Yi Ming <ofseed@foxmail.com>
2023-08-01 13:50:18 +0800

    Reconfigure navbuddy

--- lua/tool/navbuddy.lua
+++ lua/tool/navbuddy.lua
@@ -1,56 +1,5 @@
 local custom = require "custom"
 
-local opts = {
-  window = {
-    border = custom.border, -- "rounded", "double", "solid", "none"
-    -- or an array with eight chars building up the border in a clockwise fashion
-    -- starting with the top-left corner. eg: { "╔", "═" ,"╗", "║", "╝", "═", "╚", "║" }.
-    size = "60%", -- Or table format example: { height = "40%", width = "100%"}
-    position = "50%", -- Or table format example: { row = "100%", col = "0%"}
-    scrolloff = nil, -- scrolloff value within navbuddy window
-    sections = {
-      left = {
-        size = "20%",
-        border = nil, -- You can set border style for each section individually as well.
-      },
-      mid = {
-        size = "40%",
-        border = nil,
-      },
-      right = {
-        -- No size option for right most section. It fills to
-        -- remaining area.
-        border = nil,
-        preview = "leaf", -- Right section can show previews too.
-        -- Options: "leaf", "always" or "never"
-      },
-    },
-  },
-  node_markers = {
-    enabled = true,
-    icons = {
-      leaf = "  ",
-      leaf_selected = " → ",
-      branch = " ",
-    },
-  },
-  icons = custom.icons.kind_with_space,
-  use_default_mappings = true, -- If set to false, only mappings set
-  -- by user are set. Else default
-  -- mappings are used for keys
-  -- that are not set by user
-  lsp = {
-    auto_attach = true, -- If set to true, you don't need to manually use attach function
-    preference = nil, -- list of lsp server names in order of preference
-  },
-  source_buffer = {
-    follow_node = true, -- Keep the current node in focus on the source buffer
-    highlight = true, -- Highlight the currently focused node
-    reorient = "smart", -- "smart", "top", "mid" or "none"
-    scrolloff = nil, -- scrolloff value when navbuddy is open
-  },
-}
-
 return {
   "SmiteshP/nvim-navbuddy",
   dependencies = {
@@ -58,7 +7,19 @@ return {
     "SmiteshP/nvim-navic",
     "MunifTanjim/nui.nvim",
   },
-  opts = opts,
+  opts = {
+    window = {
+      border = custom.border,
+      size = {
+        height = "60%",
+        width = "80%",
+      },
+    },
+    icons = custom.icons.kind_with_space,
+    lsp = {
+      auto_attach = true,
+    },
+  },
   keys = {
     {
       "<leader>n",
