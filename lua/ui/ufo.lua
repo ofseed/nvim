6c7660c155530b394e72196a1d05648083a7174b
Yi Ming <ofseed@foxmail.com>
2023-08-02 19:13:31 +0800

    Reconfigure ufo

--- lua/ui/ufo.lua
+++ lua/ui/ufo.lua
@@ -1,17 +1,16 @@
--- //TODO: https://github.com/kevinhwang91/nvim-ufo/issues/4
--- //TODO: https://github.com/neovim/neovim/pull/17446
-vim.o.foldcolumn = "1"
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
-
-local opts = {
-  close_fold_kinds = { "imports" },
-}
-
 return {
   "kevinhwang91/nvim-ufo",
   event = "VeryLazy",
   dependencies = "kevinhwang91/promise-async",
-  opts = opts,
+  init = function()
+    vim.o.foldcolumn = "1"
+    vim.o.foldlevel = 99
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
+  end,
+  config = function()
+    require("ufo").setup {
+      close_fold_kinds = { "imports" },
+    }
+  end,
 }
