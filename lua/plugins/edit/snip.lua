944263d01b6bac4e094e6d55c64cfcca0161d39a
Yi Ming <ofseed@foxmail.com>
2023-08-03 11:57:09 +0800

    Add global helper functions

--- lua/plugins/edit/snip.lua
+++ lua/plugins/edit/snip.lua
@@ -8,8 +8,11 @@ return {
     local snip = require "luasnip"
     local types = require "luasnip.util.types"
 
+    local i = snip.insert_node
+    local sn = snip.snippet_node
+
     snip.setup {
-      update_events = {"TextChanged", "TextChangedI"},
+      update_events = { "TextChanged", "TextChangedI" },
       ext_opts = {
         [types.choiceNode] = {
           active = {
@@ -31,6 +34,19 @@ return {
             virt_text_pos = "inline",
           },
         },
+      },
+      snip_env = {
+        -- Same with text node, used for function nodes
+        text_same_with = function(args)
+          return args[1]
+        end,
+
+        -- Same with text node, used for dynamic nodes
+        insert_same_with = function(args)
+          return sn(nil, {
+            i(1, args[1]),
+          })
+        end,
       },
     }
 
