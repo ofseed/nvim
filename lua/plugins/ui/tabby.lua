3da21ac25dd2dc13e3c9b6a6e943d649c5be047a
Yi Ming <ofseed@foxmail.com>
2023-08-05 17:58:22 +0800

    Add shortcut to jump between tabpages

--- lua/plugins/ui/tabby.lua
+++ lua/plugins/ui/tabby.lua
@@ -66,5 +66,15 @@ return {
         hl = theme.fill,
       }
     end)
+
+    vim.keymap.set("n", "<M-1>", "<Cmd>1tabnext<CR>", { desc = "Go to tabpage 1" })
+    vim.keymap.set("n", "<M-2>", "<Cmd>2tabnext<CR>", { desc = "Go to tabpage 2" })
+    vim.keymap.set("n", "<M-3>", "<Cmd>3tabnext<CR>", { desc = "Go to tabpage 3" })
+    vim.keymap.set("n", "<M-4>", "<Cmd>4tabnext<CR>", { desc = "Go to tabpage 4" })
+    vim.keymap.set("n", "<M-5>", "<Cmd>5tabnext<CR>", { desc = "Go to tabpage 5" })
+    vim.keymap.set("n", "<M-6>", "<Cmd>6tabnext<CR>", { desc = "Go to tabpage 6" })
+    vim.keymap.set("n", "<M-7>", "<Cmd>7tabnext<CR>", { desc = "Go to tabpage 7" })
+    vim.keymap.set("n", "<M-8>", "<Cmd>8tabnext<CR>", { desc = "Go to tabpage 8" })
+    vim.keymap.set("n", "<M-9>", "<Cmd>9tabnext<CR>", { desc = "Go to tabpage 9" })
   end,
 }
