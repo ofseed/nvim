32393943dd0a9fddbbf0ec5dce20d508defce4bd
Yi Ming <ofseed@foxmail.com>
2023-08-07 16:06:13 +0800

    Integrate flash with telescope

--- lua/plugins/efficiency/telescope.lua
+++ lua/plugins/efficiency/telescope.lua
@@ -12,7 +12,32 @@ return {
   config = function()
     local telescope = require "telescope"
 
+    local function flash(prompt_bufnr)
+      require("flash").jump {
+        pattern = "^",
+        label = { after = { 0, 0 } },
+        search = {
+          mode = "search",
+          exclude = {
+            function(win)
+              return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "TelescopeResults"
+            end,
+          },
+        },
+        action = function(match)
+          local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+          picker:set_selection(match.pos[1] - 1)
+        end,
+      }
+    end
+
     telescope.setup {
+      defaults = {
+        mappings = {
+          n = { s = flash },
+          i = { ["<c-s>"] = flash },
+        },
+      },
       pickers = {
         find_files = {
           theme = "ivy",
