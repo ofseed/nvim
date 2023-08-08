85bfe6e3a6e25179e82873a647cbbfb69a997027
Yi Ming <ofseed@foxmail.com>
2023-08-08 13:53:42 +0800

    Reconfigure autopairs

--- lua/plugins/edit/autopairs.lua
+++ lua/plugins/edit/autopairs.lua
@@ -1,14 +1,5 @@
-local opts = {
-  check_ts = true,
-  ts_config = {
-    lua = { "string" }, -- it will not add pair on that treesitter node
-    javascript = { "template_string" },
-    java = false, -- don't check treesitter on java
-  },
-}
-
 return {
   "windwp/nvim-autopairs",
-  event = "VeryLazy",
-  opts = opts,
+  event = "InsertEnter",
+  opts = {},
 }
