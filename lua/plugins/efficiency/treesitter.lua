1035de6a073ab1f043c4dc870944f19b68ad25ad
Yi Ming <ofseed@foxmail.com>
2023-08-08 13:57:16 +0800

    Reconfigure treesitter

--- lua/plugins/efficiency/treesitter.lua
+++ lua/plugins/efficiency/treesitter.lua
@@ -1,83 +1,3 @@
-local opts = {
-  ensure_installed = "all",
-  sync_install = false,
-  ignore_install = {},
-  highlight = {
-    enable = true,
-    disable = {},
-  },
-  incremental_selection = {
-    enable = false,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incrementnl = "grc",
-      node_decremental = "grm",
-    },
-  },
-  indent = {
-    enable = true,
-  },
-  textobjects = {
-    select = {
-      enable = true,
-
-      -- Automatically jump forward to textobj, similar to targets.vim
-      lookahead = true,
-
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        -- You can optionally set descriptions to the mappings (used in the desc parameter of
-        -- nvim_buf_set_keymap) which plugins like which-key display
-        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-      },
-      -- You can choose the select mode (default is charwise 'v')
-      --
-      -- Can also be a function which gets passed a table with the keys
-      -- * query_string: eg '@function.inner'
-      -- * method: eg 'v' or 'o'
-      -- and should return the mode ('v', 'V', or '<c-v>') or a table
-      -- mapping query_strings to modes.
-      selection_modes = {
-        ["@parameter.outer"] = "v", -- charwise
-        ["@function.outer"] = "V", -- linewise
-        ["@class.outer"] = "<c-v>", -- blockwise
-      },
-      -- If you set this to `true` (default is `false`) then any textobject is
-      -- extended to include preceding or succeeding whitespace. Succeeding
-      -- whitespace has priority in order to act similarly to eg the built-in
-      -- `ap`.
-      --
-      -- Can also be a function which gets passed a table with the keys
-      -- * query_string: eg '@function.inner'
-      -- * selection_mode: eg 'v'
-      -- and should return true of false
-      include_surrounding_whitespace = true,
-    },
-  },
-  autotag = {
-    enable = true,
-  },
-  rainbow = {
-    enable = false,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
-  },
-  endwise = {
-    enable = false,
-  },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
-}
-
 return {
   "nvim-treesitter/nvim-treesitter",
   event = "VeryLazy",
@@ -95,6 +15,20 @@ return {
       require("nvim-treesitter.install").prefer_git = false
     end
 
-    require("nvim-treesitter.configs").setup(opts)
+    require("nvim-treesitter.configs").setup {
+      ensure_installed = "all",
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      autotag = {
+        enable = true,
+      },
+      context_commentstring = {
+        enable = true,
+      },
+    }
   end,
 }
