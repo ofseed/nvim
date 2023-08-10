ccf38682877dc63024ddcc4aa328498c4eabe9fe
Yi Ming <ofseed@foxmail.com>
2023-08-10 14:03:58 +0800

    Lazy load treehopper only with keys

--- lua/plugins/efficiency/treehopper.lua
+++ lua/plugins/efficiency/treehopper.lua
@@ -1,6 +1,5 @@
 return {
   "mfussenegger/nvim-treehopper",
-  event = "VeryLazy",
   keys = {
     { "m", ":<C-U>lua require('tsht').nodes()<CR>", mode = "o" },
     { "m", ":lua require('tsht').nodes()<CR>", mode = "v" },
