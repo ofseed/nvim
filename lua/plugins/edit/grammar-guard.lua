959ffe330a74f7ea6f0f261ba6ca68eca5339de4
Yi Ming <ofseed@foxmail.com>
2023-08-10 12:57:32 +0800

    Remove unneeded plugins

--- lua/plugins/edit/grammar-guard.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "brymer-meneses/grammar-guard.nvim",
-  -- Now grammar check is provided by ltex languages server
-  enabled = false,
-}
--- lua/plugins/ui/scrollview.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "dstein64/nvim-scrollview",
-  enabled = false,
-  event = "VeryLazy",
-}
