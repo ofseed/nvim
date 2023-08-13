2ed9785ff16fc90058e553eb4710e50617818d8e
Yi Ming <ofseed@foxmail.com>
2023-08-13 12:57:08 +0800

    Fix set foldcolumn

--- lua/plugins/ui/ufo.lua
+++ lua/plugins/ui/ufo.lua
@@ -3,11 +3,27 @@ return {
   event = "VeryLazy",
   dependencies = "kevinhwang91/promise-async",
   init = function()
+    local set_foldcolumn_for_file = vim.api.nvim_create_augroup("set_foldcolumn_for_file", {
+      clear = true,
+    })
     vim.api.nvim_create_autocmd("BufWinEnter", {
-      desc = "Set foldcolumn when current is a file",
+      group = set_foldcolumn_for_file,
       callback = function()
         if vim.bo.buftype == "" then
           vim.wo.foldcolumn = "1"
+        else
+          vim.wo.foldcolumn = "0"
+        end
+      end,
+    })
+    vim.api.nvim_create_autocmd("OptionSet", {
+      group = set_foldcolumn_for_file,
+      pattern = "buftype",
+      callback = function()
+        if vim.bo.buftype == "" then
+          vim.wo.foldcolumn = "1"
+        else
+          vim.wo.foldcolumn = "0"
         end
       end,
     })
