db5275f4a05b24d323f88e8fcb1dc8a7808750c0
Yi Ming <ofseed@foxmail.com>
2023-08-14 09:06:20 +0800

    Reconfigure toggleterm

--- lua/plugins/tool/toggleterm.lua
+++ lua/plugins/tool/toggleterm.lua
@@ -1,59 +1,25 @@
 local custom = require "custom"
 
-local opts = {
-  -- size can be a number or function which is passed the current terminal
-  size = function(term)
-    if term.direction == "horizontal" then
-      return 15
-    elseif term.direction == "vertical" then
-      return vim.o.columns * 0.4
-    end
-  end,
-  open_mapping = [[<c-\>]],
-  on_open = nil, -- function to run when the terminal opens
-  on_close = nil, -- function to run when the terminal closes
-  on_stdout = nil, -- callback for processing output on stdout
-  on_stderr = nil, -- callback for processing output on stderr
-  on_exit = nil, -- function to run when terminal process exits
-  hide_numbers = true, -- hide the number column in toggleterm buffers
-  shade_filetypes = {},
-  autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
-  highlights = {},
-  shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
-  shading_factor = "1", -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-  start_in_insert = true,
-  insert_mappings = true, -- whether or not the open mapping applies in insert mode
-  terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
-  persist_size = true,
-  persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
-  direction = "horizontal", -- "vertical" | "horizontal" | "tab" | "float",
-  close_on_exit = true, -- close the terminal window when the process exits
-  shell = vim.uv.os_uname().sysname == "Windows" and "pwsh" or "fish", -- change the default shell
-  auto_scroll = true, -- automatically scroll to the bottom on terminal output
-  -- This field is only relevant if direction is set to 'float'
-  float_opts = {
-    -- The border key is *almost* the same as 'nvim_open_win'
-    -- see :h nvim_open_win for details on borders however
-    -- the 'curved' border is a custom border type
-    -- not natively supported but implemented in this plugin.
-    border = custom.border, -- 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
-    -- like `size`, width and height can be a number or function which is passed the current terminal
-    width = 100,
-    height = 40,
-    winblend = 8,
-  },
-  winbar = {
-    enabled = true,
-    name_formatter = function(term) --  term: Terminal
-      return term.name
-    end,
-  },
-}
-
 return {
   "akinsho/nvim-toggleterm.lua",
   version = "*",
-  opts = opts,
+  opts = {
+    size = function(term)
+      if term.direction == "horizontal" then
+        return 15
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+    end,
+    open_mapping = [[<c-\>]],
+    shell = vim.uv.os_uname().sysname == "Windows" and "pwsh" or "fish",
+    float_opts = {
+      border = custom.border,
+    },
+    winbar = {
+      enabled = true,
+    },
+  },
   keys = {
     { "<C-\\>" },
     { "<Esc>", "<C-\\><C-N>", mode = "t" },
--- lua/plugins/ui/marks.lua
+++ lua/plugins/ui/marks.lua
@@ -35,6 +35,7 @@ local opts = {
     "NeogitStatus",
     "registers",
     "dropbar_menu",
+    "toggleterm",
   },
   -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
   -- sign/virttext. Bookmarks can be used to group together positions and quickly move
