5050a1f38a2a3b01e7dc2525418636c4e366a9a2
Yi Ming <ofseed@foxmail.com>
2023-08-22 20:49:39 +0800

    Reconfigure package-info.nvim

--- lua/plugins/tool/package-info.lua
+++ lua/plugins/tool/package-info.lua
@@ -1,25 +1,3 @@
-local opts = {
-  colors = {
-    up_to_date = "#3C4048", -- Text color for up to date package virtual text
-    outdated = "#d19a66", -- Text color for outdated package virtual text
-  },
-  icons = {
-    enable = true, -- Whether to display icons
-    style = {
-      up_to_date = "|  ", -- Icon for up to date packages
-      outdated = "|  ", -- Icon for outdated packages
-    },
-  },
-  autostart = true, -- Whether to autostart when `package.json` is opened
-  hide_up_to_date = true, -- It hides up to date versions when displaying virtual text
-  hide_unstable_versions = false, -- It hides unstable versions from version list e.g next-11.1.3-canary3
-  -- Can be `npm` or `yarn`. Used for `delete`, `install` etc...
-  -- The plugin will try to auto-detect the package manager based on
-  -- `yarn.lock` or `package-lock.json`. If none are found it will use the
-  -- provided one, if nothing is provided it will use `yarn`
-  package_manager = "npm",
-}
-
 return {
   "vuki656/package-info.nvim",
   event = {
@@ -27,8 +5,11 @@ return {
     "BufRead package-lock.json",
   },
   dependencies = "MunifTanjim/nui.nvim",
-  opts = opts,
   config = function()
+    require("package-info").setup {
+      package_manager = "pnpm",
+    }
+
     vim.keymap.set("n", "<localleader>s", function()
       require("package-info").show()
     end, { desc = "Show package versions" })
