a50020b6f5ba122b4e6febe3a8e93740d1591f32
Yi Ming <ofseed@foxmail.com>
2023-08-25 00:32:52 +0800

    Add keymap for treesitter playground

--- lua/plugins/treesitter/playground.lua
+++ lua/plugins/treesitter/playground.lua
@@ -4,4 +4,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
   },
   cmd = { "TSPlaygroundToggle" },
+  keys = {
+    { "<leader>tp", "<Cmd>TSPlaygroundToggle<CR>", desc = "Treesitter Playground" },
+  },
 }
