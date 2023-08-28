f506b3402b3c27de508c71bfa85b51f0509cbe38
Yi Ming <ofseed@foxmail.com>
2023-08-28 13:08:27 +0800

    Set diagnostic icons for pqf

--- lua/plugins/ui/pqf.lua
+++ lua/plugins/ui/pqf.lua
@@ -1,4 +1,12 @@
+local custom = require "custom"
+
+local signs = vim.tbl_extend("keep", {
+  warning = custom.icons.diagnostic.warn,
+}, custom.icons.diagnostic)
+
 return {
   "yorickpeterse/nvim-pqf",
-  opts = {},
+  opts = {
+    signs = signs,
+  },
 }
