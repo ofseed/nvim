14b77486466bb743b38ce085d7088a94607ade5b
Yi Ming <ofseed@foxmail.com>
2023-08-30 01:52:07 +0800

    Add tint

---
+++ lua/plugins/ui/tint.lua
@@ -0,0 +1,29 @@
+return {
+  "levouh/tint.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("tint").setup {
+      highlight_ignore_patterns = {
+        "WinSeparator",
+        "EndOfBuffer",
+        "LineNr",
+        "IndentBlanklineChar",
+      },
+      window_ignore_function = function(win)
+        local buf = vim.api.nvim_win_get_buf(win)
+        local buftype = vim.api.nvim_buf_get_option(buf, "buftype")
+        return buftype ~= ""
+      end,
+    }
+
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      desc = "Refresh tint",
+      callback = function()
+        -- Timeout is needed for wait all colorscheme highlights applied
+        vim.defer_fn(function()
+          require("tint").refresh()
+        end, 100)
+      end,
+    })
+  end,
+}
