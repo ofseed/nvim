188972e5ef5e1931a05450537e210b0d915672c7
Yi Ming <ofseed@foxmail.com>
2023-08-30 01:53:35 +0800

    Reconfigure regexplainer

--- lua/plugins/ui/regexplainer.lua
+++ lua/plugins/ui/regexplainer.lua
@@ -1,32 +1,18 @@
-local opts = {
-  -- 'narrative'
-  mode = "narrative", -- TODO: 'ascii', 'graphical'
-
-  -- automatically show the explainer when the cursor enters a regexp
-  auto = false,
-
-  -- Whether to log debug messages
-  debug = false,
-
-  -- 'split', 'popup'
-  display = "popup",
-
-  mappings = {
-    show = "gR",
-  },
-
-  narrative = {
-    separator = "\n",
-  },
-}
+local custom = require "custom"
 
 return {
   "bennypowers/nvim-regexplainer",
   -- Not very useful now
-  enabled = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
   },
-  opts = opts,
+  keys = { "gR", desc = "Toggle Regexplainer" },
+  opts = {
+    popup = {
+      border = {
+        style = custom.border,
+      },
+    },
+  },
 }
