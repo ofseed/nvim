aaa507d29e92fb26c11903dba77095865285598b
Yi Ming <ofseed@foxmail.com>
2023-08-30 03:16:51 +0800

    Make tink darker

--- lua/plugins/ui/tint.lua
+++ lua/plugins/ui/tint.lua
@@ -3,6 +3,7 @@ return {
   event = "VeryLazy",
   config = function()
     require("tint").setup {
+      tint = -60,
       highlight_ignore_patterns = {
         "WinSeparator",
         "EndOfBuffer",
