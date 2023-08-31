4f23dfcd0342fadbfe9c44d159bde4c57e61bf8e
Yi Ming <ofseed@foxmail.com>
2023-08-31 19:21:23 +0800

    Add flash treesitter keymaps

--- lua/plugins/efficiency/flash.lua
+++ lua/plugins/efficiency/flash.lua
@@ -2,4 +2,22 @@ return {
   "folke/flash.nvim",
   opts = true,
   event = "VeryLazy",
+  keys = {
+    {
+      "s",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+    {
+      "S",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Treesitter Search",
+    },
+  },
 }
