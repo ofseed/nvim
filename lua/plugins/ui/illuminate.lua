5946e4102189e8fd728f62d688ddde2e40c765da
Yi Ming <ofseed@foxmail.com>
2023-09-01 22:41:39 +0800

    Move autocmd to saperate file

--- lua/plugins/ui/illuminate.lua
+++ lua/plugins/ui/illuminate.lua
@@ -60,17 +60,6 @@ return {
   config = function()
     local illuminate = require "illuminate"
     illuminate.configure(opts)
-    -- Highlight on yank
-    -- conflict with vim-illuminate
-    vim.api.nvim_create_autocmd("TextYankPost", {
-      group = vim.api.nvim_create_augroup("highlight_on_yank", {}),
-      desc = "Briefly highlight yanked text",
-      callback = function()
-        illuminate.pause()
-        vim.highlight.on_yank()
-        illuminate.resume()
-      end,
-    })
   end,
   keys = {
     {
