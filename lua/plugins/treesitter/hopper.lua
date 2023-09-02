a12dc855edbfe0d7382a52e2ebba123fa35eecee
Yi Ming <ofseed@foxmail.com>
2023-09-03 01:54:46 +0800

    Disable treehopper since there is flash

--- lua/plugins/treesitter/hopper.lua
+++ lua/plugins/treesitter/hopper.lua
@@ -1,5 +1,6 @@
 return {
   "mfussenegger/nvim-treehopper",
+  enabled = false,
   keys = {
     { "m", ":<C-U>lua require('tsht').nodes()<CR>", mode = "o" },
     { "m", ":lua require('tsht').nodes()<CR>", mode = "v" },
