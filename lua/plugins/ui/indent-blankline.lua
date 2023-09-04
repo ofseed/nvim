d31905be3f337ef781c0951cf5369dd07492e0a4
Yi Ming <ofseed@foxmail.com>
2023-09-04 22:09:30 +0800

    Migrate to indent-blankline v3

--- lua/plugins/ui/indent-blankline.lua
+++ lua/plugins/ui/indent-blankline.lua
@@ -1,16 +1,9 @@
-local opts = {
-  show_current_context = true,
-  show_current_context_start = true,
-  -- use_treesitter = true,
-  -- use_treesitter_scope = true,
-  filetype_exclude = {
-    "help",
-    "OverseerForm",
-  },
-}
-
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = "VeryLazy",
-  opts = opts,
+  branch = "v3",
+  opts = {},
+  config = function(_, opts)
+    require("ibl").setup(opts)
+  end,
 }
