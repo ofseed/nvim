a8e8d951a3a0e50df18c15c7064f6c13debdff4a
Yi Ming <ofseed@foxmail.com>
2023-09-05 11:07:46 +0800

    Add scope highlight from rainbow-delimiters

--- lua/plugins/ui/indent-blankline.lua
+++ lua/plugins/ui/indent-blankline.lua
@@ -1,9 +1,28 @@
+-- Highlight group from rainbow-delimiters
+local highlight = {
+  "RainbowDelimiterRed",
+  "RainbowDelimiterYellow",
+  "RainbowDelimiterBlue",
+  "RainbowDelimiterOrange",
+  "RainbowDelimiterGreen",
+  "RainbowDelimiterViolet",
+  "RainbowDelimiterCyan",
+}
+
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = "VeryLazy",
   branch = "v3",
-  opts = {},
+  opts = {
+    scope = {
+      enabled = true,
+      highlight = highlight,
+    },
+  },
   config = function(_, opts)
     require("ibl").setup(opts)
+
+    local hooks = require "ibl.hooks"
+    hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
   end,
 }
