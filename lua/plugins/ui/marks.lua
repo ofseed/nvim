38f4f9fda0cd93b8e0cfaeb3c0a383a4451f0eb5
Yi Ming <ofseed@foxmail.com>
2023-09-11 21:40:45 +0800

    Disable mark signs in `OverseerForm`

--- lua/plugins/ui/marks.lua
+++ lua/plugins/ui/marks.lua
@@ -36,6 +36,7 @@ local opts = {
     "registers",
     "dropbar_menu",
     "toggleterm",
+    "OverseerForm",
   },
   -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
   -- sign/virttext. Bookmarks can be used to group together positions and quickly move
