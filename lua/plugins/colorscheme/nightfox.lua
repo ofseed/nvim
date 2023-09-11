d96b2e2ee6c0b4d0edf47303d3858b99603c7e06
Yi Ming <ofseed@foxmail.com>
2023-09-11 12:30:18 +0800

    Reconfigure nightfox

--- lua/plugins/colorscheme/nightfox.lua
+++ lua/plugins/colorscheme/nightfox.lua
@@ -1,40 +1,12 @@
-local opts = {
-  options = {
-    -- Compiled file's destination location
-    compile_path = vim.fs.joinpath(vim.fn.stdpath "cache", "nightfox"),
-    compile_file_suffix = "_compiled", -- Compiled file suffix
-    transparent = false, -- Disable setting background
-    terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-    dim_inactive = false, -- Non focused panes set to alternative background
-    module_default = true, -- Default enable value for modules
-    styles = { -- Style to be applied to different syntax groups
-      comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
-      conditionals = "NONE",
-      constants = "NONE",
-      functions = "NONE",
-      keywords = "NONE",
-      numbers = "NONE",
-      operators = "NONE",
-      strings = "NONE",
-      types = "NONE",
-      variables = "NONE",
-    },
-    inverse = { -- Inverse highlight for different types
-      match_paren = false,
-      visual = false,
-      search = false,
-    },
-    modules = { -- List of various plugins and additional options
-      -- ...
-    },
-  },
-  palettes = {},
-  specs = {},
-  groups = {},
-}
-
 return {
   "EdenEast/nightfox.nvim",
   lazy = true,
-  opts = opts,
+  opts = {
+    options = {},
+    groups = {
+      all = {
+        MatchParen = { fg = "NONE" },
+      },
+    },
+  },
 }
