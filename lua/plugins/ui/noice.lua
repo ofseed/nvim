149635b1a4b353cff1df62e2f89654673364a5dc
Yi Ming <ofseed@foxmail.com>
2023-09-17 20:33:00 +0800

    Enable noice signature

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -31,9 +31,6 @@ return {
         ["vim.lsp.util.stylize_markdown"] = true,
         ["cmp.entry.get_documentation"] = true,
       },
-      signature = {
-        enabled = false,
-      },
       message = {
         enabled = false,
       },
