12a7943ade03469fed1534a966de1a4f68102bcd
Yi Ming <ofseed@foxmail.com>
2023-09-17 20:38:29 +0800

    Set pumheight to 12

--- init.lua
+++ init.lua
@@ -30,6 +30,7 @@ vim.o.conceallevel = 1
 vim.o.scrolloff = 2
 vim.o.sidescrolloff = 5
 vim.o.pumblend = 12
+vim.o.pumheight = 12
 vim.o.fillchars = "foldopen:,foldclose:,foldsep: ,diff:╱"
 vim.o.textwidth = 80
 vim.o.guifont = "Cascadia Code PL:h14"
