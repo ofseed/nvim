8233067c1d9e750641cccab6c88aa70cbeb98ba9
Yi Ming <ofseed@foxmail.com>
2023-09-17 18:31:30 +0800

    Revert "Move autocmd to saperate file"
    This reverts commit 5946e4102189e8fd728f62d688ddde2e40c765da.

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,7 +1,5 @@
 local group = vim.api.nvim_create_augroup("ofseed", {})
 
-local _, illuminate = pcall(require, "illuminate")
-
 vim.api.nvim_create_autocmd({
   "FocusGained",
   "BufEnter",
@@ -13,15 +11,5 @@ vim.api.nvim_create_autocmd({
     if vim.fn.getcmdwintype() == "" then
       vim.cmd "checktime"
     end
-  end,
-})
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-  group = vim.api.nvim_create_augroup("highlight_on_yank", {}),
-  desc = "Highlight yanked text",
-  callback = function()
-    pcall(illuminate.pause)
-    vim.highlight.on_yank()
-    pcall(illuminate.resume)
   end,
 })
