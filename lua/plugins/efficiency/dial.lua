7b4f8c008a38bb5ff95eede14c351c7d8868da72
Yi Ming <ofseed@foxmail.com>
2023-09-19 10:35:21 +0800

    Update dial.nvim

--- lua/plugins/efficiency/dial.lua
+++ lua/plugins/efficiency/dial.lua
@@ -2,40 +2,57 @@ return {
   "monaqa/dial.nvim",
   keys = {
     {
+      "n",
       "<C-a>",
       function()
-        require("dial.map").inc_normal()()
+        require("dial.map").manipulate("increment", "normal")
       end,
     },
     {
       "<C-x>",
       function()
-        require("dial.map").dec_normal()()
-      end,
-    },
-    {
-      "<C-a>",
-      function()
-        require("dial.map").inc_visual()()
-      end,
-    },
-    {
-      "<C-x>",
-      function()
-        require("dial.map").dec_visual()()
+        require("dial.map").manipulate("decrement", "normal")
       end,
     },
     {
       "g<C-a>",
       function()
-        require("dial.map").inc_gvisual()()
+        require("dial.map").manipulate("increment", "gnormal")
       end,
     },
     {
       "g<C-x>",
       function()
-        require("dial.map").dec_gvisual()()
+        require("dial.map").manipulate("decrement", "gnormal")
       end,
+    },
+    {
+      "<C-a>",
+      function()
+        require("dial.map").manipulate("increment", "visual")
+      end,
+      mode = "v",
+    },
+    {
+      "<C-x>",
+      function()
+        require("dial.map").manipulate("decrement", "visual")
+      end,
+      mode = "v",
+    },
+    {
+      "g<C-a>",
+      function()
+        require("dial.map").manipulate("increment", "gvisual")
+      end,
+      mode = "v",
+    },
+    {
+      "g<C-x>",
+      function()
+        require("dial.map").manipulate("decrement", "gvisual")
+      end,
+      mode = "v",
     },
   },
 }
