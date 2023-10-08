2a466d9a88d89c608c74d9e0687425e766edf802
Yi Ming <ofseed@foxmail.com>
2023-10-08 13:10:27 +0800

    Reconfigure statuscol

--- lua/plugins/ui/statuscol.lua
+++ lua/plugins/ui/statuscol.lua
@@ -1,31 +1,29 @@
-local config = function()
-
-local statuscol = require "statuscol"
-
-local builtin = require "statuscol.builtin"
-
-statuscol.setup {
-  bt_ignore = { "nofile", "terminal" },
-  segments = {
-    {
-      sign = { name = { ".*" } },
-      click = "v:lua.ScSa",
-    },
-    {
-      text = { builtin.lnumfunc },
-      click = "v:lua.ScLa",
-    },
-    {
-      sign = { name = { "GitSigns" }, colwidth = 1, wrap = true },
-      click = "v:lua.ScSa",
-    },
-    { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-  },
-}
-
-end
-
 return {
   "luukvbaal/statuscol.nvim",
-  config = config,
+  opts = function()
+    local builtin = require "statuscol.builtin"
+
+    return {
+      bt_ignore = { "nofile", "terminal" },
+      segments = {
+        {
+          sign = { name = { ".*" } },
+          click = "v:lua.ScSa",
+        },
+        {
+          text = { builtin.lnumfunc },
+          condition = { true, builtin.not_empty },
+          click = "v:lua.ScLa",
+        },
+        {
+          sign = { name = { "GitSigns" }, colwidth = 1, wrap = true },
+          click = "v:lua.ScSa",
+        },
+        {
+          text = { builtin.foldfunc },
+          click = "v:lua.ScFa",
+        },
+      },
+    }
+  end,
 }
