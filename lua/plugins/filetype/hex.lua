b1bbe5e505d6325aa9463b5ca00d95c950a97b3e
Yi Ming <ofseed@foxmail.com>
2023-10-16 19:52:51 +0800

    Reconfigure hex

--- lua/plugins/filetype/hex.lua
+++ lua/plugins/filetype/hex.lua
@@ -1,25 +1,4 @@
-local opts = {
-
-  -- cli command used to dump hex data
-  dump_cmd = "xxd -g 1 -u",
-
-  -- cli command used to assemble from hex data
-  assemble_cmd = "xxd -r",
-
-  -- function that runs on BufReadPre to determine if it's binary or not
-  is_buf_binary_pre_read = function()
-    -- logic that determines if a buffer contains binary data or not
-    -- must return a bool
-  end,
-
-  -- function that runs on BufReadPost to determine if it's binary or not
-  is_buf_binary_post_read = function()
-    -- logic that determines if a buffer contains binary data or not
-    -- must return a bool
-  end,
-}
-
 return {
   "RaafatTurki/hex.nvim",
-  opts = opts,
+  opts = {},
 }
