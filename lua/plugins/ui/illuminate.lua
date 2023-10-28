f68958db0f5969038bd76b9eccefa4bbee2ee32d
Yi Ming <ofseed@foxmail.com>
2023-10-28 19:09:47 +0800

    Reconfigure illuminate

--- lua/plugins/ui/illuminate.lua
+++ lua/plugins/ui/illuminate.lua
@@ -1,65 +1,10 @@
-local opts = {
-  -- providers: provider used to get references in the buffer, ordered by priority
-  providers = {
-    "lsp",
-    "treesitter",
-    "regex",
-  },
-  -- delay: delay in milliseconds
-  delay = 100,
-  -- filetype_overrides: filetype specific overrides.
-  -- The keys are strings to represent the filetype while the values are tables that
-  -- supports the same keys passed to .configure except for filetypes_denylist and filetypes_allowlist
-  filetype_overrides = {},
-  -- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
-  filetypes_denylist = {
-    "dirvish",
-    "fugitive",
-    "NvimTree",
-    "Outline",
-    "aerial",
-    "packer",
-    "alpha",
-    "NeogitStatus",
-    "NeogitPopup",
-    "org",
-    "orghelp",
-  },
-  -- filetypes_allowlist: filetypes to illuminate, this is overriden by filetypes_denylist
-  filetypes_allowlist = {},
-  -- modes_denylist: modes to not illuminate, this overrides modes_allowlist
-  -- See `:help mode()` for possible values
-  modes_denylist = {},
-  -- modes_allowlist: modes to illuminate, this is overriden by modes_denylist
-  -- See `:help mode()` for possible values
-  modes_allowlist = {},
-  -- providers_regex_syntax_denylist: syntax to not illuminate, this overrides providers_regex_syntax_allowlist
-  -- Only applies to the 'regex' provider
-  -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
-  providers_regex_syntax_denylist = {},
-  -- providers_regex_syntax_allowlist: syntax to illuminate, this is overriden by providers_regex_syntax_denylist
-  -- Only applies to the 'regex' provider
-  -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
-  providers_regex_syntax_allowlist = {},
-  -- under_cursor: whether or not to illuminate under the cursor
-  under_cursor = true,
-  -- large_file_cutoff: number of lines at which to use large_file_config
-  -- The `under_cursor` option is disabled when this cutoff is hit
-  large_file_cutoff = nil,
-  -- large_file_config: config to use for large files (based on large_file_cutoff).
-  -- Supports the same keys passed to .configure
-  -- If nil, vim-illuminate will be disabled for large files.
-  large_file_overrides = nil,
-  -- min_count_to_highlight: minimum number of matches required to perform highlighting
-  min_count_to_highlight = 1,
-}
-
 return {
   "RRethy/vim-illuminate",
   event = "VeryLazy",
   config = function()
     local illuminate = require "illuminate"
-    illuminate.configure(opts)
+    illuminate.configure {}
+
     -- Highlight on yank
     -- conflict with vim-illuminate
     vim.api.nvim_create_autocmd("TextYankPost", {
