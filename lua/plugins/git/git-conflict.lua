095889e31a9862895091ee9a16758c8c06c11600
Yi Ming <ofseed@foxmail.com>
2023-11-27 17:25:21 +0800

    Reconfigure git-conflict

--- lua/plugins/git/git-conflict.lua
+++ lua/plugins/git/git-conflict.lua
@@ -1,14 +1,14 @@
-local opts = {
-  default_mappings = true, -- disable buffer local mapping created by this plugin
-  disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
-  highlights = { -- They must have background color, otherwise the default color will be used
-    incoming = "DiffText",
-    current = "DiffAdd",
-  },
-}
-
 return {
   "akinsho/git-conflict.nvim",
-  opts = opts,
   event = "VeryLazy",
+  opts = {
+    default_mappings = {
+      ours = "co",
+      theirs = "ct",
+      none = "c0",
+      both = "cb",
+      next = "]x",
+      prev = "[x",
+    },
+  },
 }
