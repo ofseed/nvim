1acd9d2b992671c8efbb41f1508b5acd5babf2f1
Yi Ming <ofseed@foxmail.com>
2023-11-30 15:15:48 +0800

    Manage dap-ui layouts instead of fallback to default

--- lua/plugins/dap/dap-ui.lua
+++ lua/plugins/dap/dap-ui.lua
@@ -6,6 +6,44 @@ return {
     floating = {
       border = custom.border,
     },
+    layouts = {
+      {
+        elements = {
+          {
+            id = "scopes",
+            size = 0.25,
+          },
+          {
+            id = "breakpoints",
+            size = 0.25,
+          },
+          {
+            id = "stacks",
+            size = 0.25,
+          },
+          {
+            id = "watches",
+            size = 0.25,
+          },
+        },
+        position = "left",
+        size = 40,
+      },
+      {
+        elements = {
+          {
+            id = "repl",
+            size = 0.5,
+          },
+          {
+            id = "console",
+            size = 0.5,
+          },
+        },
+        position = "bottom",
+        size = 10,
+      },
+    },
   },
   keys = {
     {
