7257bb73bbc037b69a9c3c5115e4b7e6b0d82b97
Yi Ming <ofseed@foxmail.com>
2023-11-30 11:38:06 +0800

    Add an extra key to toggle different layouts

--- lua/plugins/dap/dap-ui.lua
+++ lua/plugins/dap/dap-ui.lua
@@ -11,7 +11,14 @@ return {
     {
       "<leader>du",
       function()
-        require("dapui").toggle()
+        require("dapui").toggle { layout = 1 }
+      end,
+      desc = "Toggle UI",
+    },
+    {
+      "<leader>dU",
+      function()
+        require("dapui").toggle { layout = 2 }
       end,
       desc = "Toggle UI",
     },
