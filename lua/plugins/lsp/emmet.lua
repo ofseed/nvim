75d4159550ea0df3b2656ad1e6bf62705aa93b3d
Yi Ming <ofseed@foxmail.com>
2023-12-11 12:30:50 +0800

    Reconfigure emmet to avoid redundant localleader keymap

--- lua/plugins/lsp/emmet.lua
+++ lua/plugins/lsp/emmet.lua
@@ -1,11 +1,21 @@
 return {
   "olrtg/nvim-emmet",
-  keys = {
-    {
-      "<localleader>x",
-      function()
-        require("nvim-emmet").wrap_with_abbreviation()
+  lazy = true,
+  init = function()
+    vim.api.nvim_create_autocmd("LspAttach", {
+      desc = "Setup nvim-emmet",
+      callback = function(args)
+        local bufnr = args.buf
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+        if client == nil or client.name ~= "emmet_language_server" then
+          return
+        end
+
+        vim.keymap.set("n", "<localleader>x", function()
+          require("nvim-emmet").wrap_with_abbreviation()
+        end, { buffer = bufnr, desc = "Wrap with abbreviation" })
       end,
-    },
-  },
+    })
+  end,
 }
