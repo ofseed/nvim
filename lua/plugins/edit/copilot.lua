03e4f1c63e58b7ad8f02443463826561483685a7
Yi Ming <ofseed@foxmail.com>
2023-12-12 18:33:27 +0800

    Reconfigure copilot

--- lua/plugins/edit/copilot.lua
+++ lua/plugins/edit/copilot.lua
@@ -1,25 +1,21 @@
-local config = function()
-
-vim.g.copilot_filetypes = {
-  registers = 0,
-  markdown = 1,
-}
-
-vim.keymap.set(
-  "i",
-  "<C-J>",
-  'copilot#Accept("<CR>")',
-  { silent = true, script = true, expr = true, replace_keycodes = false }
-)
-vim.keymap.set("i", "<M-NL>", "<Cmd>Copilot<CR>")
-
-end
-
 return {
   "github/copilot.vim",
   event = "VeryLazy",
-  init = function ()
+  init = function()
     vim.g.copilot_no_tab_map = true
   end,
-  config = config,
+  config = function()
+    vim.g.copilot_filetypes = {
+      registers = 0,
+      markdown = 1,
+    }
+
+    vim.keymap.set(
+      "i",
+      "<C-J>",
+      'copilot#Accept("<CR>")',
+      { silent = true, script = true, expr = true, replace_keycodes = false }
+    )
+    vim.keymap.set("i", "<M-NL>", "<Cmd>Copilot<CR>")
+  end,
 }
