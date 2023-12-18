8afacc1a137231f6675c4a576fdeac34e5a594a2
Yi Ming <ofseed@foxmail.com>
2023-12-18 13:25:59 +0800

    Show whether a file is modified in dropbar

--- lua/plugins/ui/dropbar.lua
+++ lua/plugins/ui/dropbar.lua
@@ -17,5 +17,15 @@ return {
         symbols = custom.icons.kind_with_space,
       },
     },
+    sources = {
+      path = {
+        modified = function(sym)
+          return sym:merge {
+            name = sym.name .. " [+]",
+            name_hl = "DiffAdded",
+          }
+        end,
+      },
+    },
   },
 }
