7375c0156421ee00a266ec055044e3e772feeaf1
Yi Ming <ofseed@foxmail.com>
2023-12-18 18:56:12 +0800

    Add proper sign highlight for `DapStopped`

--- lua/plugins/dap/dap.lua
+++ lua/plugins/dap/dap.lua
@@ -1,6 +1,6 @@
 vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticError" })
 vim.fn.sign_define("DapLogPoint", { text = "", texthl = "DiagnosticInfo" })
-vim.fn.sign_define("DapStopped", { text = "", texthl = "Constant" })
+vim.fn.sign_define("DapStopped", { text = "", texthl = "Constant", linehl = "debugPC" })
 vim.fn.sign_define("DapBreakpointRejected", { text = "" })
 
 return {
