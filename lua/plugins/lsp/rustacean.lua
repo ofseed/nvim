840536d8044e4c5af81523231a5ec4da4155b1d7
Yi Ming <ofseed@foxmail.com>
2024-01-03 19:20:38 +0800

    Do not showDisassembly

--- lua/plugins/lsp/rustacean.lua
+++ lua/plugins/lsp/rustacean.lua
@@ -2,4 +2,28 @@
 return {
   "mrcjkb/rustaceanvim",
   ft = { "rust" },
+  config = function()
+    vim.g.rustaceanvim = function()
+      return {
+        dap = {
+          adapter = {
+            type = "server",
+            port = "${port}",
+            host = "127.0.0.1",
+            executable = {
+              command = "codelldb",
+              args = {
+                "--port",
+                "${port}",
+                "--settings",
+                vim.json.encode {
+                  showDisassembly = "never",
+                },
+              },
+            },
+          },
+        },
+      }
+    end
+  end,
 }
