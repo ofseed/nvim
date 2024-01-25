42f37224d943cada8f4091d070e75cd12514824e
Yi Ming <ofseed@foxmail.com>
2024-01-25 12:16:48 +0000

    Add new module path for magick

--- lua/plugins/tool/image.lua
+++ lua/plugins/tool/image.lua
@@ -1,5 +1,9 @@
 return {
   "3rd/image.nvim",
+  init = function()
+    package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.nix-profile/share/lua/5.1/?/init.lua;"
+    package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.nix-profile/share/lua/5.1/?.lua;"
+  end,
   event = "VeryLazy",
   opts = {},
 }
