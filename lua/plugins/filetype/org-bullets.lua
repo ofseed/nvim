e72b42ff643536ca2379450850118bb0aa8a301a
Yi Ming <ofseed@foxmail.com>
2024-01-27 16:03:59 +0000

    Reconfigure orgmode

--- lua/plugins/filetype/org-bullets.lua
+++ lua/plugins/filetype/org-bullets.lua
@@ -1,26 +1,7 @@
-local opts = {
-  concealcursor = false, -- If false then when the cursor is on a line underlying characters are visible
-  symbols = {
-    -- list symbol
-    list = "•",
-    -- headlines can be a list
-    headlines = { "◉", "○", "✸", "✿" },
-    -- or a function that receives the defaults and returns a list
-    -- headlines = function(default_list)
-    --   table.insert(default_list, "♥")
-    --   return default_list
-    -- end,
-    checkboxes = {
-      half = { "", "OrgTSCheckboxHalfChecked" },
-      done = { "✓", "OrgDone" },
-      todo = { "˟", "OrgTODO" },
-    },
-  },
-}
-
+---@type LazyPluginSpec
 return {
   "akinsho/org-bullets.nvim",
   enabled = false,
   ft = { "org" },
-  opts = opts,
+  opts = {},
 }
--- lua/plugins/tool/orgmode.lua
+++ lua/plugins/tool/orgmode.lua
@@ -1,14 +1,15 @@
-local opts = {
-  org_agenda_files = { "~/Documents/org/*" },
-  org_default_notes_file = "~/Documents/org/refile.org",
-}
-
+---@type LazyPluginSpec
 return {
   "nvim-orgmode/orgmode",
   enabled = false,
   event = "VeryLazy",
-  opts = opts,
-  config = function()
-    require("orgmode").setup_ts_grammar()
+  opts = {
+    org_agenda_files = { "~/org/*" },
+    org_default_notes_file = "~/org/refile.org",
+  },
+  config = function(_, opts)
+    local orgmode = require "orgmode"
+    orgmode.setup_ts_grammar()
+    orgmode.setup(opts)
   end,
 }
