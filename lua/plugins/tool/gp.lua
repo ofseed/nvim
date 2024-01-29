77e69fdacad355cd28dc83298af61653616908b0
Yi Ming <ofseed@foxmail.com>
2024-01-29 17:03:20 +0000

    Lazy load gp.nvim

--- lua/plugins/tool/gp.lua
+++ lua/plugins/tool/gp.lua
@@ -1,5 +1,10 @@
+---@type LazyPluginSpec
 return {
   "https://github.com/Robitx/gp.nvim",
   cond = vim.env.OPENAI_API_KEY ~= nil,
+  event = "VeryLazy",
   opts = {},
+  config = function(_, opts)
+    require("gp").setup(opts)
+  end,
 }
