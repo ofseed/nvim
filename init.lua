071a166b08dc3c3c58e4b84d629b63820deecd3c
Yi Ming <ofseed@foxmail.com>
2024-01-29 18:14:24 +0000

    Remove textwidth, only use colorcolumn

--- init.lua
+++ init.lua
@@ -34,8 +34,7 @@ vim.o.smoothscroll = true
 vim.o.pumblend = 12
 vim.o.pumheight = 12
 vim.o.fillchars = "diff:╱,foldopen:,foldclose:"
-vim.o.textwidth = 80
-vim.o.colorcolumn = "+1"
+vim.o.colorcolumn = "81"
 vim.o.guifont = "Cascadia Code PL:h14"
 
 -- Keymapping
