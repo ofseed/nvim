5157254c788b5e2f61a83ba430d6fd30ffc0196e
Yi Ming <ofseed@foxmail.com>
2024-01-29 17:56:21 +0000

    Modify gp.nvim display style

--- lua/plugins/tool/gp.lua
+++ lua/plugins/tool/gp.lua
@@ -1,10 +1,13 @@
+local custom = require "custom"
+
 ---@type LazyPluginSpec
 return {
   "https://github.com/Robitx/gp.nvim",
   cond = vim.env.OPENAI_API_KEY ~= nil,
   event = "VeryLazy",
-  opts = {},
-  config = function(_, opts)
-    require("gp").setup(opts)
-  end,
+  opts = {
+    toggle_target = "split",
+    style_chat_finder_border = custom.border,
+    style_popup_border = custom.border,
+  },
 }
