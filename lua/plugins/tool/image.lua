a29593a957358e1b88008372c2cbd6bfc95ff6e0
Yi Ming <ofseed@foxmail.com>
2024-02-01 06:54:49 +0000

    Disable image.nvim on Windows

--- lua/plugins/tool/image.lua
+++ lua/plugins/tool/image.lua
@@ -1,5 +1,6 @@
 return {
   "3rd/image.nvim",
+  enabled = vim.uv.os_uname().sysname ~= "Windows_NT",
   init = function()
     package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.nix-profile/share/lua/5.1/?/init.lua;"
     package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.nix-profile/share/lua/5.1/?.lua;"
