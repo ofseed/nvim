1e3262dde81da5dffaea494d11ce34e9cc191149
Yi Ming <ofseed@foxmail.com>
2024-02-05 05:34:16 +0000

    Use default dropbar enable handler

--- lua/plugins/ui/dropbar.lua
+++ lua/plugins/ui/dropbar.lua
@@ -3,15 +3,6 @@ local custom = require "custom"
 return {
   "Bekaboo/dropbar.nvim",
   opts = {
-    general = {
-      enable = function(buf, win)
-        return not vim.api.nvim_win_get_config(win).zindex
-          and vim.bo[buf].buftype == ""
-          and vim.api.nvim_buf_get_name(buf) ~= ""
-          and not vim.wo[win].diff
-          and vim.bo[buf].filetype ~= "toggleterm"
-      end,
-    },
     icons = {
       kinds = {
         symbols = custom.icons.kind_with_space,
