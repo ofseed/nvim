162f7833d23594d9b2a1162a2e640fe19c2cc3a6
Yi Ming <ofseed@foxmail.com>
2024-02-13 14:22:57 +0000

    Lazy load image.nvim with filetypes

--- lua/plugins/tool/image.lua
+++ lua/plugins/tool/image.lua
@@ -1,10 +1,11 @@
+---@type LazyPluginSpec
 return {
   "3rd/image.nvim",
   enabled = vim.uv.os_uname().sysname ~= "Windows_NT",
+  ft = { "markdown", "org", "norg" },
   init = function()
     package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.nix-profile/share/lua/5.1/?/init.lua;"
     package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.nix-profile/share/lua/5.1/?.lua;"
   end,
-  event = "VeryLazy",
   opts = {},
 }
