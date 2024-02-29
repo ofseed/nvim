0b1f6c097c05a5352e30c7d8b7f2e667a1b24f84
Yi Ming <ofseed@foxmail.com>
2024-02-29 15:59:22 +0000

    Set fzf colors inside fzf-lua

--- lua/plugins/tool/fzf.lua
+++ lua/plugins/tool/fzf.lua
@@ -5,6 +5,21 @@ return {
   "ibhagwan/fzf-lua",
   cmd = { "FzfLua" },
   opts = {
+    fzf_colors = {
+      ["fg"] = { "fg", "CursorLine" },
+      ["bg"] = { "bg", "Normal" },
+      ["hl"] = { "fg", "Statement" },
+      ["fg+"] = { "fg", "Normal" },
+      ["bg+"] = { "bg", "CursorLine" },
+      ["hl+"] = { "fg", "Statement" },
+      ["info"] = { "fg", "PreProc" },
+      ["prompt"] = { "fg", "Conditional" },
+      ["pointer"] = { "fg", "Exception" },
+      ["marker"] = { "fg", "Keyword" },
+      ["spinner"] = { "fg", "Label" },
+      ["header"] = { "fg", "Comment" },
+      ["gutter"] = { "bg", "Normal" },
+    },
     winopts = {
       border = custom.border,
     },
