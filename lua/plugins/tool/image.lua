152fe2d273ef8f412621ae9292ee9e97310a2090
Yi Ming <ofseed@foxmail.com>
2024-03-04 09:17:57 +0000

    Load image.nvim when opening hijacked files

--- lua/plugins/tool/image.lua
+++ lua/plugins/tool/image.lua
@@ -3,9 +3,19 @@ return {
   "3rd/image.nvim",
   enabled = vim.uv.os_uname().sysname ~= "Windows_NT",
   ft = { "markdown", "org", "norg" },
+  event = function(plugin)
+    return {
+      {
+        event = "BufRead",
+        pattern = plugin.opts.hijack_file_patterns,
+      },
+    }
+  end,
   init = function()
     package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.nix-profile/share/lua/5.1/?/init.lua;"
     package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.nix-profile/share/lua/5.1/?.lua;"
   end,
-  opts = {},
+  opts = {
+    hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" },
+  },
 }
