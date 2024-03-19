741b187b6fd2f1590bc9856bf4a603e89170fc8a
Yi Ming <ofseed@foxmail.com>
2024-03-19 13:47:55 +0800

    Update rest.nvim config

--- lua/plugins/filetype/rest.lua
+++ lua/plugins/filetype/rest.lua
@@ -1,45 +1,12 @@
-local opts = {
-  -- Open request results in a horizontal split
-  result_split_horizontal = false,
-  -- Keep the http file buffer above|left when split horizontal|vertical
-  result_split_in_place = false,
-  -- Skip SSL verification, useful for unknown certificates
-  skip_ssl_verification = false,
-  -- Encode URL before making request
-  encode_url = true,
-  -- Highlight request on run
-  highlight = {
-    enabled = true,
-    timeout = 150,
-  },
-  result = {
-    -- toggle showing URL, HTTP info, headers at top the of result window
-    show_url = true,
-    show_http_info = true,
-    show_headers = true,
-    -- executables or functions for formatting response body [optional]
-    -- set them to nil if you want to disable them
-    formatters = {
-      json = "jq",
-      html = function(body)
-        return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
-      end,
-    },
-  },
-  -- Jump to request line on run
-  jump_to_request = false,
-  env_file = ".env",
-  custom_dynamic_variables = {},
-  yank_dry_run = true,
-}
-
+---@type LazyPluginSpec
 return {
   "rest-nvim/rest.nvim",
   ft = { "http" },
-  opts = opts,
-  config = function()
-    vim.keymap.set("n", "<localleader>r", "<Plug>RestNvim<CR>", { silent = true, desc = "Rest" })
-    vim.keymap.set("n", "<localleader>p", "<Plug>RestNvimPreview<CR>", { silent = true, desc = "Rest preview" })
-    vim.keymap.set("n", "<localleader>l", "<Plug>RestNvimLast<CR>", { silent = true, desc = "Rest last" })
+  opts = {},
+  config = function(_, opts)
+    require("rest-nvim").setup(opts)
+
+    vim.keymap.set("n", "<localleader>r", "<Cmd>Rest run<CR>", { silent = true, desc = "Run request under the cursor" })
+    vim.keymap.set("n", "<localleader>l", "<Cmd>Rest run last<CR>", { silent = true, desc = "Re-run latest request" })
   end,
 }
