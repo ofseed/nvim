1538892f0063eac5f68d8f19ec9e5006d0c20b55
Yi Ming <ofseed@foxmail.com>
2024-03-20 10:59:33 +0800

    Use vscode style snippets from vscode-go instead

--- lua/plugins/edit/snip.lua
+++ lua/plugins/edit/snip.lua
@@ -91,5 +91,6 @@ return {
     require("luasnip.loaders.from_lua").lazy_load {
       paths = vim.fn.stdpath "config" .. "/lua/snippets",
     }
+    require("luasnip.loaders.from_vscode").lazy_load()
   end,
 }
