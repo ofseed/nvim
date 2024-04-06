58b18dabf86b57f013e1d13df093a027505da093
Yi Ming <ofseed@foxmail.com>
2024-04-06 20:56:49 +0800

    Disable lsp-lens

--- lua/plugins/ui/lsp-lens.lua
+++ lua/plugins/ui/lsp-lens.lua
@@ -1,6 +1,9 @@
+---@type LazyPluginSpec
 return {
   "VidocqH/lsp-lens.nvim",
   event = "LspAttach",
+  -- Bad performance
+  enabled = false,
   config = function()
     require("lsp-lens").setup {}
     vim.api.nvim_set_hl(0, "LspLens", { link = "LspCodeLens" })
