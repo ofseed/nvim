04574d950fc4724449deb889d8ed5cd413929636
Yi Ming <ofseed@foxmail.com>
2024-04-13 22:21:53 +0800

    Update copilot name

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -17,7 +17,7 @@ local function lsp()
       return client.attached_buffers[buf]
     end)
     :filter(function(client)
-      return client.name ~= "copilot"
+      return client.name ~= "GitHub Copilot"
     end)
     :map(function(client)
       return " " .. client.name
