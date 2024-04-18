71593bf87fd4b2741594a46596c1d6ac9d9a0e88
Yi Ming <ofseed@foxmail.com>
2024-04-18 17:42:23 +0800

    Reconfigure vimtex

--- lua/plugins/filetype/tex.lua
+++ lua/plugins/filetype/tex.lua
@@ -1,24 +1,21 @@
-local config = function()
-
-vim.g.vimtex_mappings_prefix = "<localleader>"
-vim.g.tex_flavor = "latex"
-vim.g.vimtex_quickfix_mode = 0
-vim.g.vimtex_compiler_latexmk_engines = { _ = "-xelatex" }
-vim.g.vimtex_compiler_latexrun_engines = { _ = "xelatex" }
-vim.g.vimtex_compiler_progname = "nvr"
-vim.g.vimtex_toc_config = {
-  name = "TOC",
-  layers = { "content", "todo", "include" },
-  split_width = 25,
-  todo_sorted = 0,
-  show_help = 1,
-  show_numbers = 1,
-}
-
-end
-
 return {
   "lervag/vimtex",
   ft = { "tex" },
-  config = config,
+  config = function()
+    vim.g.tex_flavor = "latex"
+
+    vim.g.vimtex_mappings_prefix = "<localleader>"
+    vim.g.vimtex_quickfix_mode = 0
+    vim.g.vimtex_compiler_latexmk_engines = { _ = "-xelatex" }
+    vim.g.vimtex_compiler_latexrun_engines = { _ = "xelatex" }
+    vim.g.vimtex_compiler_progname = "nvr"
+    vim.g.vimtex_toc_config = {
+      name = "TOC",
+      layers = { "content", "todo", "include" },
+      split_width = 25,
+      todo_sorted = 0,
+      show_help = 1,
+      show_numbers = 1,
+    }
+  end,
 }
