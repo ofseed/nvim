4a0793e587ce527d1f520fe68f6a6d9ecf5ab13b
Yi Ming <ofseed@foxmail.com>
2024-04-24 20:09:30 +0800

    Add screenkey.nvim

---
+++ lua/plugins/tool/screenkey.lua
@@ -0,0 +1,12 @@
+local custom = require "custom"
+
+---@type LazyPluginSpec
+return {
+  "NStefan002/screenkey.nvim",
+  cmd = "Screenkey",
+  opts = {
+    win_opts = {
+      border = custom.border,
+    },
+  },
+}
