e641fd7219dae6abe722b3d9f6706348d8127859
Yi Ming <ofseed@foxmail.com>
2024-05-29 11:00:36 +0800

    Add copilot chat

---
+++ lua/plugins/tool/copilot-chat.lua
@@ -0,0 +1,17 @@
+---@type LazyPluginSpec
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  branch = "canary",
+  cmd = {
+    "CopilotChat",
+    "CopilotChatOpen",
+    "CopilotChatClose",
+    "CopilotChatToggle",
+    "CopilotChatStop",
+    "CopilotChatReset",
+    "CopilotChatSave",
+    "CopilotChatLoad",
+    "CopilotChatDebugInfo",
+  },
+  opts = {},
+}
