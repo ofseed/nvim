7bfb3ae31e4ecd41fa558c4607dd10d332b04051
Yi Ming <ofseed@foxmail.com>
2024-06-06 19:37:31 +0800

    Lazy load autotag by filetype

--- lua/plugins/treesitter/autotag.lua
+++ lua/plugins/treesitter/autotag.lua
@@ -3,6 +3,13 @@ local locals = require "locals"
 ---@type LazyPluginSpec
 return {
   "windwp/nvim-ts-autotag",
+  ft = {
+    "html",
+    "javascriptreact",
+    "typescriptreact",
+    "vue",
+    "svelte",
+  },
   cond = not locals.treesitter_dev,
   -- Cannot be lazy loaded
   opts = {},
