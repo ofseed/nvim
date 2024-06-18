8385e2b51785c053397c119c7946ed4ca7c6d476
Yi Ming <ofseed@foxmail.com>
2024-06-18 11:35:19 +0800

    Use main branch of nvim-treesitter-textobjects

--- lua/plugins/treesitter/textobjects.lua
+++ lua/plugins/treesitter/textobjects.lua
@@ -1,94 +1,314 @@
-local locals = require "locals"
-
 ---@type LazyPluginSpec
 return {
   "nvim-treesitter/nvim-treesitter-textobjects",
-  branch = locals.treesitter_dev and "main" or "master",
-  event = "VeryLazy",
+  branch = "main",
   dependencies = {
     { "nvim-treesitter/nvim-treesitter" },
     { "ghostbuster91/nvim-next" },
   },
   opts = {
     select = {
-      enable = true,
       lookahead = true,
-      keymaps = {
-        ["aa"] = { query = "@parameter.outer", desc = "a argument" },
-        ["ia"] = {
-          query = "@parameter.inner",
-          desc = "inner part of a argument",
-        },
-        ["af"] = { query = "@function.outer", desc = "a function region" },
-        ["if"] = {
-          query = "@function.inner",
-          desc = "inner part of a function region",
-        },
-        ["ar"] = { query = "@return.outer", desc = "a return" },
-        ["ir"] = { query = "@return.outer", desc = "inner return" },
-        ["ac"] = { query = "@class.outer", desc = "a of a class" },
-        ["ic"] = {
-          query = "@class.inner",
-          desc = "inner part of a class region",
-        },
-        ["aj"] = { query = "@conditional.outer", desc = "a judge" },
-        ["ij"] = {
-          query = "@conditional.inner",
-          desc = "inner part of a judge region",
-        },
-        ["al"] = { query = "@loop.outer", desc = "a loop" },
-        ["il"] = { query = "@loop.inner", desc = "inner part of a loop" },
-      },
       include_surrounding_whitespace = true,
     },
     move = {
-      enable = true,
       set_jumps = true,
-      goto_next_start = {
-        ["]a"] = { query = "@parameter.outer", desc = "Next argument start" },
-        ["]f"] = { query = "@function.outer", desc = "Next function start" },
-        ["]r"] = { query = "@function.outer", desc = "Next return start" },
-        ["]c"] = { query = "@class.outer", desc = "Next class start" },
-        ["]j"] = { query = "@conditional.outer", desc = "Next judge start" },
-        ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
-      },
-      goto_next_end = {
-        ["]A"] = { query = "@parameter.outer", desc = "Next argument end" },
-        ["]F"] = { query = "@function.outer", desc = "Next function end" },
-        ["]R"] = { query = "@function.outer", desc = "Next return end" },
-        ["]C"] = { query = "@class.outer", desc = "Next class end" },
-        ["]J"] = { query = "@conditional.outer", desc = "Next judge end" },
-        ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
-      },
-      goto_previous_start = {
-        ["[a"] = {
-          query = "@parameter.outer",
-          desc = "Previous argument start",
-        },
-        ["[f"] = { query = "@function.outer", desc = "Previous function start" },
-        ["[r"] = { query = "@function.outer", desc = "Previous return start" },
-        ["[c"] = { query = "@class.outer", desc = "Previous class start" },
-        ["[j"] = { query = "@conditional.outer", desc = "Previous judge start" },
-        ["[l"] = { query = "@loop.outer", desc = "Previous loop start" },
-      },
-      goto_previous_end = {
-        ["[A"] = { query = "@parameter.outer", desc = "Previous argument end" },
-        ["[F"] = { query = "@function.outer", desc = "Previous function end" },
-        ["[R"] = { query = "@function.outer", desc = "Previous return end" },
-        ["[C"] = { query = "@class.outer", desc = "Previous class end" },
-        ["[J"] = { query = "@conditional.outer", desc = "Previous judge end" },
-        ["[L"] = { query = "@loop.outer", desc = "Previous loop end" },
-      },
     },
   },
-  config = function(_, opts)
-    if not locals.treesitter_dev then
-      require("nvim-treesitter.configs").setup {
-        textobjects = opts,
-      }
-      return
-    end
-
-    require("nvim-treesitter-textobjects").setup(opts)
-  end,
+  keys = {
+    --
+    -- Select
+    --
+    {
+      "aa",
+      function()
+        require("nvim-treesitter-textobjects.select").select_textobject "@parameter.outer"
+      end,
+      mode = { "x", "o" },
+      desc = "a argument",
+    },
+    {
+      "ia",
+      function()
+        require("nvim-treesitter-textobjects.select").select_textobject "@parameter.inner"
+      end,
+      mode = { "x", "o" },
+      desc = "inner part of a argument",
+    },
+    {
+      "af",
+      function()
+        require("nvim-treesitter-textobjects.select").select_textobject "@function.outer"
+      end,
+      mode = { "x", "o" },
+      desc = "a function region",
+    },
+    {
+      "if",
+      function()
+        require("nvim-treesitter-textobjects.select").select_textobject "@function.inner"
+      end,
+      mode = { "x", "o" },
+      desc = "inner part of a function region",
+    },
+    {
+      "ar",
+      function()
+        require("nvim-treesitter-textobjects.select").select_textobject "@return.outer"
+      end,
+      mode = { "x", "o" },
+      desc = "a return",
+    },
+    {
+      "ir",
+      function()
+        require("nvim-treesitter-textobjects.select").select_textobject "@return.outer"
+      end,
+      mode = { "x", "o" },
+      desc = "inner return",
+    },
+    {
+      "ac",
+      function()
+        require("nvim-treesitter-textobjects.select").select_textobject "@class.outer"
+      end,
+      mode = { "x", "o" },
+      desc = "a of a class",
+    },
+    {
+      "ic",
+      function()
+        require("nvim-treesitter-textobjects.select").select_textobject "@class.inner"
+      end,
+      mode = { "x", "o" },
+      desc = "inner part of a class region",
+    },
+    {
+      "aj",
+      function()
+        require("nvim-treesitter-textobjects.select").select_textobject "@conditional.outer"
+      end,
+      mode = { "x", "o" },
+      desc = "a judge",
+    },
+    {
+      "ij",
+      function()
+        require("nvim-treesitter-textobjects.select").select_textobject "@conditional.inner"
+      end,
+      mode = { "x", "o" },
+      desc = "inner part of a judge region",
+    },
+    {
+      "al",
+      function()
+        require("nvim-treesitter-textobjects.select").select_textobject "@loop.outer"
+      end,
+      mode = { "x", "o" },
+      desc = "a loop",
+    },
+    {
+      "il",
+      function()
+        require("nvim-treesitter-textobjects.select").select_textobject "@loop.inner"
+      end,
+      mode = { "x", "o" },
+      desc = "inner part of a loop",
+    },
+    --
+    -- Move
+    --
+    {
+      "]a",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_next_start "@parameter.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Next argument start",
+    },
+    {
+      "]A",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_next_end "@parameter.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Next argument end",
+    },
+    {
+      "[a",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_previous_start "@parameter.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Previous argument start",
+    },
+    {
+      "[A",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_previous_end "@parameter.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Previous argument end",
+    },
+    {
+      "]f",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_next_start "@function.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Next function start",
+    },
+    {
+      "]F",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_next_end "@function.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Next function end",
+    },
+    {
+      "[f",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_previous_start "@function.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Previous function start",
+    },
+    {
+      "[F",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_previous_end "@function.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Previous function end",
+    },
+    {
+      "]r",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_next_start "@return.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Next return start",
+    },
+    {
+      "]R",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_next_end "@return.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Next return end",
+    },
+    {
+      "[r",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_previous_start "@return.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Previous return start",
+    },
+    {
+      "[R",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_previous_end "@return.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Previous return end",
+    },
+    {
+      "]c",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_next_start "@class.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Next class start",
+    },
+    {
+      "]C",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_next_end "@class.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Next class end",
+    },
+    {
+      "[c",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_previous_start "@class.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Previous class start",
+    },
+    {
+      "[C",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_previous_end "@class.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Previous class end",
+    },
+    {
+      "]j",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_next_start "@conditional.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Next judge start",
+    },
+    {
+      "]J",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_next_end "@conditional.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Next judge end",
+    },
+    {
+      "[j",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_previous_start "@conditional.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Previous judge start",
+    },
+    {
+      "[J",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_previous_end "@conditional.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Previous judge end",
+    },
+    {
+      "]l",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_next_start "@loop.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Next loop start",
+    },
+    {
+      "]L",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_next_end "@loop.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Next loop end",
+    },
+    {
+      "[l",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_previous_start "@loop.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Previous loop start",
+    },
+    {
+      "[L",
+      function()
+        require("nvim-treesitter-textobjects.move").goto_previous_end "@loop.outer"
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Previous loop end",
+    },
+  },
 }
