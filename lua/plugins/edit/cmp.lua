bd9319ecb7966b87161159bed8bfe1bbb6b29f0f
Yi Ming <ofseed@foxmail.com>
2024-07-30 19:52:54 +0800

    Fix wrong keymap cmp

--- lua/plugins/edit/cmp.lua
+++ lua/plugins/edit/cmp.lua
@@ -53,7 +53,7 @@ return {
         mapping = {
           ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
           ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-          ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+          ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
           ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
           ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
           ["<C-e>"] = cmp.mapping {
