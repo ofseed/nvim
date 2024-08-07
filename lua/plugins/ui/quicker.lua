88ba64cd0a9aeb53fd7d1f45d732ee742475167c
Yi Ming <ofseed@foxmail.com>
2024-08-07 23:30:52 +0800

    Replace pqf with quicker

---
+++ lua/plugins/ui/quicker.lua
@@ -0,0 +1,44 @@
+---@type LazyPluginSpec
+return {
+  "stevearc/quicker.nvim",
+  lazy = false,
+  ---@module "quicker"
+  ---@type quicker.SetupOptions
+  opts = {
+    keys = {
+      {
+        "zo",
+        function()
+          require("quicker").expand {
+            before = 2,
+            after = 2,
+            add_to_existing = true,
+          }
+        end,
+        desc = "Expand quickfix context",
+      },
+      {
+        "zc",
+        function()
+          require("quicker").collapse()
+        end,
+        desc = "Collapse quickfix context",
+      },
+    },
+    highlight = {
+      lsp = false,
+    },
+    max_filename_width = function()
+      return math.floor(math.max(80, vim.o.columns / 3))
+    end,
+  },
+  keys = {
+    {
+      "<leader>q",
+      function()
+        require("quicker").toggle()
+      end,
+      desc = "Toggle quickfix",
+    },
+  },
+}
