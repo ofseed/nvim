b83420b7b3aae116c0364accdc124d342363c88c
Yi Ming <ofseed@foxmail.com>
2024-09-06 01:25:51 +0800

    Load lua packages on nixos

--- lua/plugins/tool/image.lua
+++ lua/plugins/tool/image.lua
@@ -2,6 +2,16 @@
 return {
   "3rd/image.nvim",
   enabled = vim.uv.os_uname().sysname ~= "Windows_NT",
+  init = function()
+    local hostname = vim.uv.os_gethostname()
+    if hostname == "nixos" then
+      package.path = table.concat({
+        package.path,
+        vim.env.HOME .. "/.nix-profile/share/lua/5.1/?/init.lua",
+        vim.env.HOME .. "/.nix-profile/share/lua/5.1/?.lua",
+      }, ";")
+    end
+  end,
   dependencies = {
     { "leafo/magick" },
   },
