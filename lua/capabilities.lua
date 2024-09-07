774fa7cc2488786c7dbdd425e20c07f6f15fed82
Yi Ming <ofseed@foxmail.com>
2024-09-07 14:02:37 +0800

    Mannually manage LSP capabilities

--- lua/capabilities.lua
+++ lua/capabilities.lua
@@ -1,16 +1,55 @@
 -- Default LSP server settigns
 local M = vim.lsp.protocol.make_client_capabilities()
 
-local ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if not ok then
-  vim.notify "Could not load nvim-cmp"
-  return
-end
+-- Additional capabilities supported by nvim-cmp
+M.textDocument.completion = {
+  dynamicRegistration = false,
+  completionItem = {
+    snippetSupport = true,
+    commitCharactersSupport = true,
+    deprecatedSupport = true,
+    preselectSupport = true,
+    tagSupport = {
+      valueSet = {
+        1, -- Deprecated
+      },
+    },
+    insertReplaceSupport = true,
+    resolveSupport = {
+      properties = {
+        "documentation",
+        "detail",
+        "additionalTextEdits",
+        "sortText",
+        "filterText",
+        "insertText",
+        "textEdit",
+        "insertTextFormat",
+        "insertTextMode",
+      },
+    },
+    insertTextModeSupport = {
+      valueSet = {
+        1, -- asIs
+        2, -- adjustIndentation
+      },
+    },
+    labelDetailsSupport = true,
+  },
+  contextSupport = true,
+  insertTextMode = 1,
+  completionList = {
+    itemDefaults = {
+      "commitCharacters",
+      "editRange",
+      "insertTextFormat",
+      "insertTextMode",
+      "data",
+    },
+  },
+}
 
--- Add additional capabilities supported by nvim-cmp
-M = cmp_nvim_lsp.default_capabilities(M)
-
--- Enable LSP folddingRange capability
+-- Additional capabilities supported by nvim-ufo
 M.textDocument.foldingRange = {
   dynamicRegistration = false,
   lineFoldingOnly = true,
