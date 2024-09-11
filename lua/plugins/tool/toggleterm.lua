d92c64e9f196a0446a73e2cd35105b539564225d
Yi Ming <ofseed@foxmail.com>
2024-09-11 10:21:42 +0800

    Make sure toggleterm is lazy loaded

--- lua/plugins/tool/toggleterm.lua
+++ lua/plugins/tool/toggleterm.lua
@@ -30,36 +30,10 @@ return {
       vim.keymap.set("t", "<Esc>", "<Nop>", { buffer = bufnr })
     end
 
-    local lazygit = require("toggleterm.terminal").Terminal:new {
-      cmd = "lazygit",
-      hidden = true,
-      direction = "float",
-      float_opts = float_opts,
-      on_create = on_create,
-    }
-    local lazydocker = require("toggleterm.terminal").Terminal:new {
-      cmd = "lazydocker",
-      hidden = true,
-      direction = "float",
-      float_opts = float_opts,
-      on_create = on_create,
-    }
-    local gh_dash = require("toggleterm.terminal").Terminal:new {
-      -- https://github.com/dlvhdr/gh-dash/issues/316
-      env = { LANG = "en_US.UTF-8" },
-      cmd = "gh-dash",
-      hidden = true,
-      direction = "float",
-      float_opts = float_opts,
-      on_create = on_create,
-    }
-    local lf = require("toggleterm.terminal").Terminal:new {
-      cmd = "lf",
-      hidden = true,
-      direction = "float",
-      float_opts = float_opts,
-      on_create = on_create,
-    }
+    local lazygit
+    local lazydocker
+    local gh_dash
+    local lf
 
     return {
       { "<C-\\>" },
@@ -74,6 +48,15 @@ return {
       {
         "<leader>gl",
         function()
+          if not lazygit then
+            lazygit = require("toggleterm.terminal").Terminal:new {
+              cmd = "lazygit",
+              hidden = true,
+              direction = "float",
+              float_opts = float_opts,
+              on_create = on_create,
+            }
+          end
           lazygit:toggle()
         end,
         desc = "LazyGit",
@@ -81,6 +64,15 @@ return {
       {
         "<leader>pd",
         function()
+          if not lazydocker then
+            lazydocker = require("toggleterm.terminal").Terminal:new {
+              cmd = "lazydocker",
+              hidden = true,
+              direction = "float",
+              float_opts = float_opts,
+              on_create = on_create,
+            }
+          end
           lazydocker:toggle()
         end,
         desc = "Lazy Docker",
@@ -88,6 +80,17 @@ return {
       {
         "<leader>pg",
         function()
+          if not gh_dash then
+            gh_dash = require("toggleterm.terminal").Terminal:new {
+              -- https://github.com/dlvhdr/gh-dash/issues/316
+              env = { LANG = "en_US.UTF-8" },
+              cmd = "gh-dash",
+              hidden = true,
+              direction = "float",
+              float_opts = float_opts,
+              on_create = on_create,
+            }
+          end
           gh_dash:toggle()
         end,
         desc = "GitHub Dash",
@@ -95,6 +98,15 @@ return {
       {
         "<leader>N",
         function()
+          if not lf then
+            lf = require("toggleterm.terminal").Terminal:new {
+              cmd = "lf",
+              hidden = true,
+              direction = "float",
+              float_opts = float_opts,
+              on_create = on_create,
+            }
+          end
           lf:toggle()
         end,
         desc = "File Navigator",
