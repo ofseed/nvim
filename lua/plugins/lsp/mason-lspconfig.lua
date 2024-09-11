cb1647676b31e85cc9f7c549fbf56f780e1cd9b3
Yi Ming <ofseed@foxmail.com>
2024-09-11 08:20:35 +0800

    Add zls configuration

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -76,6 +76,20 @@ return {
           }
         end,
 
+        zls = function()
+          lspconfig.zls.setup {
+            capabilities = capabilities,
+            settings = {
+              zls = {
+                inlay_hints_hide_redundant_param_names = true,
+                inlay_hints_hide_redundant_param_names_last_token = true,
+                warn_style = true,
+                highlight_global_var_declarations = true,
+              },
+            },
+          }
+        end,
+
         vtsls = function()
           local loaded, vtsls = pcall(require, "vtsls")
           if loaded then
