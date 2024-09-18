5f20e2046c8ba09e531ff2fb6b913f8eccc7ac20
Yi Ming <ofseed@foxmail.com>
2024-09-19 03:53:35 +0800

    Inline nvim-notify stages

--- lua/plugins/ui/notify.lua
+++ lua/plugins/ui/notify.lua
@@ -2,9 +2,43 @@
 return {
   "rcarriga/nvim-notify",
   event = "VeryLazy",
-  opts = {
-    render = "minimal",
-    stages = "static",
-    top_down = false,
-  },
+  opts = function()
+    local stages_util = require "notify.stages.util"
+    local direction = stages_util.DIRECTION.BOTTOM_UP
+
+    return {
+      render = "minimal",
+
+      -- Modified from "static"
+      stages = {
+        function(state)
+          local next_height = state.message.height + 2
+          local next_row = stages_util.available_slot(
+            state.open_windows,
+            next_height,
+            direction
+          )
+          if not next_row then
+            return nil
+          end
+          return {
+            relative = "editor",
+            anchor = "NE",
+            width = state.message.width,
+            height = state.message.height,
+            col = vim.opt.columns:get(),
+            row = next_row,
+            border = "rounded",
+            style = "minimal",
+          }
+        end,
+        function()
+          return {
+            col = vim.opt.columns:get(),
+            time = true,
+          }
+        end,
+      },
+    }
+  end,
 }
