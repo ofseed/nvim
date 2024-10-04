5d609ca051608f9e8f11eb27d322a06482d08707
Yi Ming <ofseed@foxmail.com>
2024-10-04 21:05:55 +0800

    Simplify dap-ui config

--- lua/plugins/dap/dap-ui.lua
+++ lua/plugins/dap/dap-ui.lua
@@ -3,6 +3,7 @@ local custom = require "custom"
 ---@type LazyPluginSpec
 return {
   "rcarriga/nvim-dap-ui",
+  ---@type dapui.Config
   opts = {
     floating = {
       border = custom.border,
@@ -11,44 +12,6 @@ return {
       collapsed = "",
       current_frame = "",
       expanded = "",
-    },
-    layouts = {
-      {
-        elements = {
-          {
-            id = "scopes",
-            size = 0.25,
-          },
-          {
-            id = "breakpoints",
-            size = 0.25,
-          },
-          {
-            id = "stacks",
-            size = 0.25,
-          },
-          {
-            id = "watches",
-            size = 0.25,
-          },
-        },
-        position = "left",
-        size = 30,
-      },
-      {
-        elements = {
-          {
-            id = "repl",
-            size = 0.5,
-          },
-          {
-            id = "console",
-            size = 0.5,
-          },
-        },
-        position = "bottom",
-        size = 10,
-      },
     },
     mappings = {
       edit = "i",
