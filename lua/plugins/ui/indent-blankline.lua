7b5b4ff665cdf90d25031c64fda0ebc3d750c8b1
Yi Ming <ofseed@foxmail.com>
2024-10-05 16:33:50 +0800

    Set indent-blankline scope highlight when rainbow-delimiters is enabled

--- lua/plugins/ui/indent-blankline.lua
+++ lua/plugins/ui/indent-blankline.lua
@@ -1,16 +1,5 @@
 local utils = require "utils"
 
--- Highlight group from rainbow-delimiters
-local highlight = {
-  "RainbowDelimiterRed",
-  "RainbowDelimiterYellow",
-  "RainbowDelimiterBlue",
-  "RainbowDelimiterOrange",
-  "RainbowDelimiterGreen",
-  "RainbowDelimiterViolet",
-  "RainbowDelimiterCyan",
-}
-
 ---@type LazyPluginSpec
 return {
   "lukas-reineke/indent-blankline.nvim",
@@ -20,19 +9,12 @@ return {
       char = "▏", -- Thiner, not suitable when enable scope
       tab_char = "▏",
     },
-    scope = {
-      highlight = highlight,
-    },
   },
   config = function(_, opts)
     local ibl = require "ibl"
     local hooks = require "ibl.hooks"
 
     ibl.setup(opts)
-    hooks.register(
-      hooks.type.SCOPE_HIGHLIGHT,
-      hooks.builtin.scope_highlight_from_extmark
-    )
 
     -- Hide first level indent, using `foldsep` to show it
     hooks.register(hooks.type.VIRTUAL_TEXT, function(_, bufnr, row, virt_text)
--- lua/plugins/ui/rainbow-delimiters.lua
+++ lua/plugins/ui/rainbow-delimiters.lua
@@ -1,3 +1,13 @@
+local highlight = {
+  "RainbowDelimiterRed",
+  "RainbowDelimiterYellow",
+  "RainbowDelimiterBlue",
+  "RainbowDelimiterOrange",
+  "RainbowDelimiterGreen",
+  "RainbowDelimiterViolet",
+  "RainbowDelimiterCyan",
+}
+
 ---@type LazyPluginSpec
 return {
   "HiPhish/rainbow-delimiters.nvim",
@@ -9,8 +19,24 @@ return {
       query = "rainbow-blocks",
       verilog = "rainbow-blocks",
     },
+    highlight = highlight,
   },
   config = function(_, opts)
+    -- Integration with indent-blankline
+    local _, ibl = pcall(require, "ibl")
+    if ibl then
+      local hooks = require "ibl.hooks"
+      ibl.update {
+        scope = {
+          highlight = highlight,
+        },
+      }
+      hooks.register(
+        hooks.type.SCOPE_HIGHLIGHT,
+        hooks.builtin.scope_highlight_from_extmark
+      )
+    end
+
     vim.g.rainbow_delimiters = opts
   end,
 }
