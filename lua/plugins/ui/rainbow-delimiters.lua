d91bc18036ad3f17199e66b913eacd892b1d12c5
Yi Ming <ofseed@foxmail.com>
2024-10-05 16:09:26 +0800

    Enable extended rainbow-blocks

--- lua/plugins/ui/rainbow-delimiters.lua
+++ lua/plugins/ui/rainbow-delimiters.lua
@@ -2,4 +2,15 @@
 return {
   "HiPhish/rainbow-delimiters.nvim",
   event = "VeryLazy",
+  opts = {
+    query = {
+      latex = "rainbow-blocks",
+      lua = "rainbow-blocks",
+      query = "rainbow-blocks",
+      verilog = "rainbow-blocks",
+    },
+  },
+  config = function(_, opts)
+    vim.g.rainbow_delimiters = opts
+  end,
 }
