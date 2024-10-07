ecffa674d18cacdae6903c01042c9a961bc0b3fb
Yi Ming <ofseed@foxmail.com>
2024-10-08 07:46:15 +0800

    Only set signs when dap is enabled

--- lua/plugins/dap/dap.lua
+++ lua/plugins/dap/dap.lua
@@ -1,21 +1,25 @@
-vim.fn.sign_define(
-  "DapBreakpoint",
-  { text = "", texthl = "DiagnosticError" }
-)
-vim.fn.sign_define(
-  "DapBreakpointCondition",
-  { text = "", texthl = "DiagnosticError" }
-)
-vim.fn.sign_define("DapLogPoint", { text = "", texthl = "DiagnosticInfo" })
-vim.fn.sign_define(
-  "DapStopped",
-  { text = "", texthl = "Constant", linehl = "debugPC" }
-)
-vim.fn.sign_define("DapBreakpointRejected", { text = "" })
-
 ---@type LazyPluginSpec
 return {
   "mfussenegger/nvim-dap",
+  init = function()
+    vim.fn.sign_define(
+      "DapBreakpoint",
+      { text = "", texthl = "DiagnosticError" }
+    )
+    vim.fn.sign_define(
+      "DapBreakpointCondition",
+      { text = "", texthl = "DiagnosticError" }
+    )
+    vim.fn.sign_define(
+      "DapLogPoint",
+      { text = "", texthl = "DiagnosticInfo" }
+    )
+    vim.fn.sign_define(
+      "DapStopped",
+      { text = "", texthl = "Constant", linehl = "debugPC" }
+    )
+    vim.fn.sign_define("DapBreakpointRejected", { text = "" })
+  end,
   dependencies = {
     "jay-babu/mason-nvim-dap.nvim",
     "LiadOz/nvim-dap-repl-highlights",
