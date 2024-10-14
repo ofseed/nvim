b81e51421d92dae1f4915ef8267dd84efe565f92
Yi Ming <ofseed@foxmail.com>
2024-10-15 02:29:25 +0800

    Add obsidian.nvim

---
+++ lua/plugins/edit/obsidian.lua
@@ -0,0 +1,19 @@
+---@type LazyPluginSpec
+return {
+  "epwalsh/obsidian.nvim",
+  event = {
+    "BufReadPre " .. vim.fn.expand "~/Documents/Obsidian/*.md",
+    "BufNewFile " .. vim.fn.expand "~/Documents/Obsidian/*.md",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "Obsidian",
+        path = "~/Documents/Obsidian",
+      },
+    },
+    picker = {
+      name = "fzf-lua",
+    },
+  },
+}
