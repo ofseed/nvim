3f87f2b651ffeeef617270264ce339b8ab76e6e2
Yi Ming <ofseed@foxmail.com>
2024-10-19 16:36:06 +0800

    Remove litee-calltree

--- lua/plugins/tool/litee-calltree.lua
+++
@@ -1,8 +0,0 @@
----@type LazyPluginSpec
-return {
-  "ldelossa/litee-calltree.nvim",
-  event = { "LspAttach" },
-  dependencies = { "ldelossa/litee.nvim" },
-  main = "litee.calltree",
-  opts = {},
-}
