189971d735b46dbdbb8183eb8e788a2df3aa8a8d
Yi Ming <ofseed@foxmail.com>
2025-02-12 00:27:10 +0800

    Nest when launched by osv

--- lua/plugins/efficiency/flatten.lua
+++ lua/plugins/efficiency/flatten.lua
@@ -1,12 +1,22 @@
 ---@type LazyPluginSpec
 return {
   "willothy/flatten.nvim",
-  opts = {
-    window = {
-      open = "alternate",
-    },
-    integrations = {
-      kitty = true,
-    },
-  },
+  opts = function()
+    local flatten = require "flatten"
+
+    ---@type Flatten.PartialConfig
+    return {
+      window = {
+        open = "alternate",
+      },
+      integrations = {
+        kitty = true,
+      },
+      hooks = {
+        should_nest = function(host)
+          return flatten.hooks.should_nest(host) or vim.env.HEADLESS_OSV
+        end,
+      },
+    }
+  end,
 }
