8ef78200bf1eae06e17bc1f4b9c3fed53d965a36
Yi Ming <ofseed@foxmail.com>
2025-05-22 22:13:31 +0800

    Fix dap-repl install

--- lua/plugins/dap/dap-repl-highlights.lua
+++ lua/plugins/dap/dap-repl-highlights.lua
@@ -3,6 +3,7 @@ local locals = require "locals"
 ---@type LazyPluginSpec
 return {
   "LiadOz/nvim-dap-repl-highlights",
+  build = ":TSInstall dap_repl",
   -- Only loads when nvim-dap is loaded
   cond = not locals.treesitter_dev,
   lazy = true,
