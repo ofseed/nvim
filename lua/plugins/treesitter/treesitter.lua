465e53f45e7ae03f17627464bc684cd986bf617b
Yi Ming <ofseed@foxmail.com>
2026-01-11 21:59:43 +0800

    Set nvim-treesitter to default settings

--- lua/plugins/treesitter/treesitter.lua
+++ lua/plugins/treesitter/treesitter.lua
@@ -4,20 +4,5 @@ return {
   branch = "main",
   event = "VeryLazy",
   build = ":TSUpdate",
-  opts = {
-    ---@diagnostic disable-next-line: param-type-mismatch
-    parser_install_dir = vim.fs.joinpath(vim.fn.stdpath "data", "site"),
-    ensure_installed = "all",
-    highlight = {
-      enable = true,
-      -- Should be controlled by vimtex
-      disable = { "latex" },
-    },
-    indent = {
-      enable = true,
-    },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter").setup(opts)
-  end,
+  opts = {},
 }
