e500d5cf6c0c57c10ba5a372b84d688e486cd791
Yi Ming <ofseed@foxmail.com>
2026-01-13 21:34:22 +0800

    Better documentation for custom autocmds

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,8 +1,9 @@
-local group = vim.api.nvim_create_augroup('ofseed', {})
+local api = vim.api
 
-vim.api.nvim_create_autocmd('FileType', {
-  group = group,
-  desc = 'Enable treesitter features for supported filetypes',
+local augroup = api.nvim_create_augroup('ofseed', {})
+
+api.nvim_create_autocmd('FileType', {
+  desc = 'Enable treesitter-based features for supported filetypes',
   callback = function(args)
     local bufnr = args.buf
     local filetype = args.match
@@ -17,76 +18,64 @@ vim.api.nvim_create_autocmd('FileType', {
       vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
     end
   end,
+  group = augroup,
 })
 
--- Indent wrapped lines based on indent settings except for ignored filetypes
-local ignored_filetypes = { 'text', 'markdown', 'org' }
----@alias option_scope "global" | "local" | nil
----@param scope option_scope
-local function set_breakindentopt(scope)
-  local indent
-  if vim.api.nvim_get_option_value('expandtab', { scope = scope }) then
-    indent = vim.api.nvim_get_option_value('shiftwidth', { scope = scope })
-  else
-    indent = vim.api.nvim_get_option_value('tabstop', { scope = scope })
-  end
-  vim.api.nvim_set_option_value('breakindentopt', 'shift:' .. indent, { scope = scope })
-end
--- This autocmd implicitly rely on `vim-sleuth` to trigger
-vim.api.nvim_create_autocmd({ 'OptionSet' }, {
-  group = group,
-  pattern = { 'expandtab', 'shiftwidth', 'tabstop' },
-  desc = "Set 'breakindentopt' based on indent settings",
-  callback = function(args)
-    local is_ignored_filetype = vim.tbl_contains(ignored_filetypes, vim.bo[args.buf].filetype)
-    ---@type option_scope
-    local scope = vim.v.option_type
-    if vim.v.option_command == 'set' and not is_ignored_filetype then
-      scope = nil
-    end
-
-    if scope == 'local' and is_ignored_filetype then
-    else
-      set_breakindentopt(scope)
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd('BufRead', {
-  group = group,
+api.nvim_create_autocmd('BufRead', {
   desc = 'Restore last cursor position',
   callback = function(args)
     local bufnr = args.buf
-    local line, col = unpack(vim.api.nvim_buf_get_mark(bufnr, '"'))
-    local winid = vim.api.nvim_get_current_win()
+    local winid = api.nvim_get_current_win()
+    local line, col = unpack(api.nvim_buf_get_mark(bufnr, '"'))
 
-    local end_line = vim.api.nvim_buf_line_count(bufnr)
-    local end_col = #vim.api.nvim_buf_get_lines(bufnr, end_line - 1, end_line, true)[1]
+    -- Avoid setting cursor outside of the buffer.
+    local end_line = api.nvim_buf_line_count(bufnr)
+    local end_col = #api.nvim_buf_get_lines(bufnr, end_line - 1, end_line, true)[1]
     if line < end_line or (line == end_line and col <= end_col) then
-      vim.api.nvim_win_set_cursor(winid, { line, col })
+      return
     end
+
+    api.nvim_win_set_cursor(winid, { line, col })
   end,
+  group = augroup,
 })
 
-vim.api.nvim_create_autocmd({
-  'FocusGained',
-  'BufEnter',
-  'CursorHold',
-}, {
-  group = group,
+api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold' }, {
   desc = 'Reload buffer on focus',
   callback = function()
     if vim.fn.getcmdwintype() == '' then
       vim.cmd 'checktime'
     end
   end,
+  group = augroup,
 })
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  --- A specific group easy to override
-  group = vim.api.nvim_create_augroup('highlight_on_yank', {}),
+api.nvim_create_autocmd('TextYankPost', {
   desc = 'Briefly highlight yanked text',
   callback = function()
     vim.highlight.on_yank()
   end,
+  group = augroup,
+})
+
+api.nvim_create_autocmd({ 'OptionSet' }, {
+  desc = "Set 'breakindentopt' based on indent settings",
+  pattern = { 'expandtab', 'shiftwidth', 'tabstop' },
+  callback = function(args)
+    if vim.tbl_contains({ 'text', 'markdown', 'org' }, vim.bo[args.buf].filetype) then
+      return
+    end
+
+    -- The scope should match how 'shiftwidth' and 'tabstop' are set.
+    local scope = vim.v.option_type
+    local indent ---@type number
+    if api.nvim_get_option_value('expandtab', { scope = scope }) then
+      indent = api.nvim_get_option_value('shiftwidth', { scope = scope })
+    else
+      indent = api.nvim_get_option_value('tabstop', { scope = scope })
+    end
+
+    api.nvim_set_option_value('breakindentopt', 'shift:' .. indent, { scope = scope })
+  end,
+  group = augroup,
 })
