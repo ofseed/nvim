9432465214181b3e6b779396042ae843af923c31
Yi Ming <ofseed@foxmail.com>
2026-02-04 13:25:48 +0800

    Disable lazydev when lua ls library is set

--- lua/plugins/lsp/lazydev.lua
+++ lua/plugins/lsp/lazydev.lua
@@ -15,5 +15,18 @@ return {
         words = { 'Lazy.*Spec' },
       },
     },
+    enabled = function(root_dir)
+      if vim.g.lazydev_enabled == false then
+        return false
+      end
+
+      -- Disable if workspace.library is set.
+      local fd = vim.uv.fs_open(root_dir .. '/.luarc.json', 'r', 438)
+      if fd then
+        local luarc = vim.json.decode(assert(vim.uv.fs_read(fd, vim.uv.fs_fstat(fd).size)))
+        return not (luarc.workspace and luarc.workspace.library)
+      end
+      return true
+    end,
   },
 }
