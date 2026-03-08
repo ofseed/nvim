9e5dfe77cb0ebcbdfb5babc3129004b2b0151c78
Yi Ming <ofseed@foxmail.com>
2026-03-08 12:18:38 +0800

    Remove luvit-meta since neovim already vendored it

--- lua/plugins/lib/luvit.lua
+++
@@ -1,5 +0,0 @@
----@type LazyPluginSpec
-return {
-  'Bilal2453/luvit-meta',
-  lazy = true,
-}
--- lua/plugins/lsp/lazydev.lua
+++ lua/plugins/lsp/lazydev.lua
@@ -7,10 +7,6 @@ return {
   opts = {
     library = {
       {
-        path = 'luvit-meta/library',
-        words = { 'vim%.uv' },
-      },
-      {
         path = 'lazy.nvim',
         words = { 'Lazy.*Spec' },
       },
