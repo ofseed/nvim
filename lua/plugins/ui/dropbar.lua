3d414cf0fd3514568195b0e921a0619122915dd3
Yi Ming <ofseed@foxmail.com>
2026-03-27 22:42:51 +0800

    Tweak winbar title for terminal buffers

--- lua/plugins/ui/dropbar.lua
+++ lua/plugins/ui/dropbar.lua
@@ -8,6 +8,17 @@ return {
     'BufNewFile',
   },
   opts = {
+    bar = {
+      update_events = {
+        buf = {
+          'BufModifiedSet',
+          'FileChangedShellPost',
+          'TextChanged',
+          'ModeChanged',
+          'TextChangedT',
+        },
+      },
+    },
     icons = {
       kinds = {
         symbols = vim.tbl_extend('keep', { Folder = ' ' }, custom.icons.kind_with_space),
@@ -20,6 +31,11 @@ return {
             name = sym.name .. ' [+]',
             name_hl = 'DiffAdded',
           }
+        end,
+      },
+      terminal = {
+        name = function(buf)
+          return vim.b[buf].term_title or vim.fn.bufname(buf)
         end,
       },
     },
